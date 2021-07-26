4c1317713901bb50188babe04dd0fda45e26e679
alexandreUser <av.souza2018@gmail.com>
2021-07-26 19:58:12 -0300

    adjusted service to fit the pattern

--- api/src/resolvers/PostResolver.ts
+++ api/src/resolvers/PostResolver.ts
@@ -2,23 +2,23 @@ import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 import Post from '../models/Post';
 import AddPostInput from '../resolvers/types/AddPostInput';
 import AddPostPayload from '../resolvers/types/AddPostPayload';
-import { createPost, findPost, listPosts } from '../services/PostService';
+import { PostProfile } from '../services/PostService';
 
 @Resolver(Post)
 export default class PostResolver {
   @Query(() => Post)
   async post(@Arg('id') id: string): Promise<Post> {
-    return await findPost(id);
+    return await PostProfile.findPost(id);
   }
 
   @Query(() => [Post])
   async posts(): Promise<Post[]> {
-    return await listPosts();
+    return await PostProfile.listPosts();
   }
 
   @Mutation(() => AddPostPayload)
   async createPost(@Arg('input') { author, body }: AddPostInput): Promise<AddPostPayload> {
-    const post = await createPost(author, body);
+    const post = await PostProfile.createPost(author, body);
     return { post };
   }
 }
--- api/src/services/PostService.ts
+++ api/src/services/PostService.ts
@@ -1,14 +1,15 @@
 import Post, { PostModel } from '../models/Post';
 
-export async function createPost(author: string, body: string): Promise<Post> {
-  const post = new PostModel({ author, body });
-  await post.save();
-  return post;
-}
-
-export async function listPosts(): Promise<Post[]> {
-  return await PostModel.find().sort({ createdAt: 'desc' }).exec();
-}
-export async function findPost(id: string): Promise<Post> {
-  return await PostModel.findByIdOrFail(id);
+export class PostProfile {
+  static async createPost(author: string, body: string): Promise<Post> {
+    const post = new PostModel({ author, body });
+    await post.save();
+    return post;
+  }
+  static async listPosts(): Promise<Post[]> {
+    return await PostModel.find().sort({ createdAt: 'desc' }).exec();
+  }
+  static async findPost(id: string): Promise<Post> {
+    return await PostModel.findByIdOrFail(id);
+  }
 }
