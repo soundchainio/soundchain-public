2a8b04042f60d094f1b9910c0ca282f67a0118d0
Caainã Jeronimo <caainaje@gmail.com>
2021-07-27 15:40:34 -0300

    remove decorator

--- api/src/models/UserEmailVerification.ts
+++ api/src/models/UserEmailVerification.ts
@@ -1,9 +1,7 @@
 import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose';
-import { ObjectType } from 'type-graphql';
 import Model from './Model';
 
 @modelOptions({ options: { customName: 'user_email_verification' } })
-@ObjectType()
 export default class UserEmailVerification extends Model {
   @prop({ required: true })
   userId: string;
