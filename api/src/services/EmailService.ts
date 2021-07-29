a2cf8a63d569022fe90f7c9a01c0356388f0f08d
Mason Seale <masonseale@Masons-MacBook-Pro.local>
2021-07-29 16:10:14 -0700

    minor EmailService refactor

--- api/src/services/EmailService.ts
+++ api/src/services/EmailService.ts
@@ -1,20 +1,26 @@
 import SendGrid from '@sendgrid/mail';
 
-const from = process.env.SENDGRID_SENDER_EMAIL as string;
+const {
+  SENDGRID_SENDER_EMAIL,
+  SENDGRID_API_KEY,
+  SENDGRID_VERIFICATION_TEMPLATE,
+  SENDGRID_RESET_PASSWORD_TEMPLATE,
+  WEB_APP_URL,
+} = process.env;
 
 export class EmailService {
   static initialize(): void {
-    SendGrid.setApiKey(process.env.SENDGRID_API_KEY as string);
+    SendGrid.setApiKey(SENDGRID_API_KEY as string);
   }
 
   static async sendEmailVerification(email: string, displayName: string, token: string): Promise<void> {
     await SendGrid.send({
       to: email,
-      from,
-      templateId: process.env.SENDGRID_VERIFICATION_TEMPLATE as string,
+      from: SENDGRID_SENDER_EMAIL as string,
+      templateId: SENDGRID_VERIFICATION_TEMPLATE as string,
       dynamicTemplateData: {
         displayName,
-        verificationLink: `${process.env.WEB_APP_URL}/verify-email?token=${token}`,
+        verificationLink: `${WEB_APP_URL}/verify-email?token=${token}`,
       },
     });
   }
@@ -22,10 +28,10 @@ export class EmailService {
   static async sendPasswordResetEmail(email: string, token: string): Promise<void> {
     await SendGrid.send({
       to: email,
-      from,
-      templateId: process.env.SENDGRID_RESET_PASSWORD_TEMPLATE as string,
+      from: SENDGRID_SENDER_EMAIL as string,
+      templateId: SENDGRID_RESET_PASSWORD_TEMPLATE as string,
       dynamicTemplateData: {
-        resetPasswordLink: `${process.env.WEB_APP_URL}/reset-password?token=${token}`,
+        resetPasswordLink: `${WEB_APP_URL}/reset-password?token=${token}`,
       },
     });
   }
