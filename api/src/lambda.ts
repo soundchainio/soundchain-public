b54f36ce40332ca2a1500d39896b4de17346230b
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2021-07-29 20:28:00 -0300

    Add api deployment stuff

---
+++ api/src/lambda.ts
@@ -0,0 +1,24 @@
+import { mongoose } from '@typegoose/typegoose';
+import { ApolloServer } from 'apollo-server-lambda';
+import type { Handler } from 'aws-lambda';
+import express from 'express';
+import { config } from './config';
+import { EmailService } from './services/EmailService';
+
+export const handler: Handler = async (...args) => {
+  await mongoose.connect(config.db.url, config.db.options);
+
+  EmailService.initialize();
+
+  const server = new ApolloServer(config.apollo);
+  const apolloHandler = server.createHandler({
+    expressAppFromMiddleware(middleware) {
+      const app = express();
+      app.use(config.express.middlewares);
+      app.use(middleware);
+      return app;
+    },
+  });
+
+  return apolloHandler(...args);
+};
---
+++ api/src/local.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import mongoose from 'mongoose';
+import { config } from './config';
+import { EmailService } from './services/EmailService';
+
+async function bootstrap() {
+  await mongoose.connect(config.db.url, config.db.options);
+
+  EmailService.initialize();
+
+  const app = express();
+  app.use(config.express.middlewares);
+
+  const server = new ApolloServer(config.apollo);
+  await server.start();
+  server.applyMiddleware({ app });
+
+  await new Promise<void>(resolve => app.listen({ port: config.express.port }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${config.express.port}${server.graphqlPath}`);
+}
+
+bootstrap();
