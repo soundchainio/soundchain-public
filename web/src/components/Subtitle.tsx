603714896140451ec6421ce04293c930dc26189a
Caainã Jeronimo <caainaje@gmail.com>
2021-08-03 20:21:36 -0300

    remove classes

--- web/src/components/Subtitle.tsx
+++ web/src/components/Subtitle.tsx
@@ -4,7 +4,7 @@ interface SubtitleProps extends React.ComponentPropsWithoutRef<'h4'> {
   children: string;
 }
 
-const baseClasses = 'text-base md:text-lg font-medium leading-7 text-white text-left truncate';
+const baseClasses = 'text-base md:text-lg font-medium text-white truncate';
 
 export const Subtitle = ({ className, children }: SubtitleProps) => (
   <h4 className={classNames(className, baseClasses)}>{children}</h4>
--- web/src/components/Title.tsx
+++ web/src/components/Title.tsx
@@ -4,7 +4,7 @@ interface TitleProps extends React.ComponentPropsWithoutRef<'h2'> {
   children: string;
 }
 
-const baseClasses = 'text-2xl md:text-xl font-black leading-7 text-white truncate';
+const baseClasses = 'text-2xl md:text-xl font-black text-white truncate';
 
 export const Title = ({ className, children }: TitleProps) => (
   <h2 className={classNames(className, baseClasses)}>{children}</h2>
