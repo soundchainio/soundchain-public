274445e032c7b23fbd75276822000a669f148d5b
Mason Seale <mason.seale@ae.studio>
2021-08-03 18:03:02 -0700

    login page components / early styles

---
+++ web/src/components/LockedLayout.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Logo from '../../public/soundchain-logo.svg';
+
+export const LockedLayout: React.FC = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gray-900 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md py-8 px-4 sm:px-10 space-y-8">
+        <div className="mx-auto h-28 w-auto relative">
+          <Image src={Logo} alt="Soundchain Logo" layout="fill" />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
