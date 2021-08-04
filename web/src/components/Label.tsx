f63ddba170c82876a7597b74363a337b095e15a3
Caainã Jeronimo <caainaje@gmail.com>
2021-08-04 10:38:17 -0300

    change h2 to span

--- web/src/components/Label.tsx
+++ web/src/components/Label.tsx
@@ -1,11 +1,11 @@
 import classNames from 'classnames';
 
-interface LabelProps extends React.ComponentPropsWithoutRef<'h2'> {
+interface LabelProps extends React.ComponentPropsWithoutRef<'span'> {
   children: string;
 }
 
 const baseClasses = 'text-sm md:text-base font-medium text-gray-600';
 
 export const Label = ({ className, children }: LabelProps) => (
-  <h2 className={classNames(className, baseClasses)}>{children}</h2>
+  <span className={classNames(className, baseClasses)}>{children}</span>
 );
