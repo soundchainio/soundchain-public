27da8ed291391d6b3552261e1982f13b03e7f781
Caainã Jeronimo <caainaje@gmail.com>
2021-08-04 16:44:56 -0300

    fix page container

--- web/src/pages/create-account.tsx
+++ web/src/pages/create-account.tsx
@@ -6,9 +6,11 @@ export default function CreateAccountPage() {
   const [step, setStep] = useState(1);
 
   return (
-    <div className="container mx-auto flex flex-col px-6 lg:px-8 bg-black min-h-screen">
-      {step === 0 && <RegisterEmailForm onSubmit={values => console.log(values)} />}
-      {step === 1 && <SetupProfileForm onSubmit={values => console.log(values)} />}
+    <div className="min-h-screen flex flex-col px-6 lg:px-8 bg-black">
+      <div className="flex flex-1 flex-col sm:mx-auto sm:w-full sm:max-w-lg bg-black">
+        {step === 0 && <RegisterEmailForm onSubmit={values => console.log(values)} />}
+        {step === 1 && <SetupProfileForm onSubmit={values => console.log(values)} />}
+      </div>
     </div>
   );
 }
