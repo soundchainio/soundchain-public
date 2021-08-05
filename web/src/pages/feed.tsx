aafabe311695289ea587b2a4f7135d090505ffbd
Caainã Jeronimo <caainaje@gmail.com>
2021-08-05 17:00:03 -0300

    create feed page

---
+++ web/src/pages/feed.tsx
@@ -0,0 +1,10 @@
+import { TopNavBar } from 'components/TopNavBar';
+
+export default function Feed() {
+  return (
+    <>
+      <TopNavBar />
+      <div className="bg-custom-black-10 min-h-screen"></div>
+    </>
+  );
+}
