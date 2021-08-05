dd0782dda88a1ef3e915d4af004ad2e0c5e790ef
Caainã Jeronimo <caainaje@gmail.com>
2021-08-05 17:43:20 -0300

    add new icons

---
+++ web/src/icons/NewPost.tsx
@@ -0,0 +1,28 @@
+import { NavIconProps } from './interfaces/NavIconProps';
+
+export const NewPost = (props: NavIconProps) => {
+  return (
+    <>
+      {props.activated && (
+        <svg width={25} height={25} viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M10 0C4.477 0 0 4.477 0 10s4.477 10 10 10 10-4.477 10-10S15.523 0 10 0zm.91 13.636a.909.909 0 01-1.82 0V10.91H6.365a.91.91 0 010-1.818H9.09V6.364a.91.91 0 111.818 0V9.09h2.727a.91.91 0 010 1.818H10.91v2.727z"
+            fill="#505050"
+          />
+        </svg>
+      )}
+      {!props.activated && (
+        <svg width={25} height={25} viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M10 0C4.477 0 0 4.477 0 10s4.477 10 10 10 10-4.477 10-10S15.523 0 10 0zm.91 13.636a.909.909 0 01-1.82 0V10.91H6.365a.91.91 0 010-1.818H9.09V6.364a.91.91 0 111.818 0V9.09h2.727a.91.91 0 010 1.818H10.91v2.727z"
+            fill="#505050"
+          />
+        </svg>
+      )}
+    </>
+  );
+};
--- web/src/pages/feed.tsx
+++ web/src/pages/feed.tsx
@@ -1,10 +1,12 @@
+import { BottomNavBar } from 'components/BottomNavBar';
 import { TopNavBar } from 'components/TopNavBar';
 
 export default function Feed() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <TopNavBar />
-      <div className="bg-custom-black-10 min-h-screen"></div>
-    </>
+      <div className="bg-custom-black-10 flex flex-1 flex-grow"></div>
+      <BottomNavBar />
+    </div>
   );
 }
