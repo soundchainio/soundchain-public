413ffd76602fbdf310637e0efab2a6f5d8df648f
Caainã Jeronimo <caainaje@gmail.com>
2021-08-05 16:59:55 -0300

    add logo

---
+++ web/src/components/TopNavBar.tsx
@@ -0,0 +1,26 @@
+import { MenuIcon } from '@heroicons/react/outline';
+import { Logo } from 'icons/Logo';
+
+export const TopNavBar = () => {
+  return (
+    <nav className="bg-gray-20">
+      <>
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <span className="sr-only">Open main menu</span>
+                <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex-shrink-0 flex items-center">
+                <Logo className="block h-8 w-auto" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    </nav>
+  );
+};
