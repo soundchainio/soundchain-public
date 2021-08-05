d92199eb0f39d075652d5cb75e77efe1c9b15536
Jean Mayer <jean.mayer@laura-br.com>
2021-08-05 14:51:55 -0300

    Changing SVG icons to Heroicons

--- web/src/components/PostActions.tsx
+++ web/src/components/PostActions.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import { RefreshIcon, ShareIcon, ThumbUpIcon, ChatAltIcon } from '@heroicons/react/solid';
 import { PostLikeOptions } from './PostLikeOptions';
 
 export const PostActions = () => {
     const [likeOptionsOpened, setLikeOptionsOpened] = useState(false);
-    const sharedClasses = "text-white text-sm text-gray-400 text-center font-bold flex-1 flex justify-center";
+    const sharedClasses = "text-white text-sm text-gray-400 text-center font-bold flex-1 flex justify-center px-1";
 
     const handleLikeButton = () => {
         setLikeOptionsOpened(!likeOptionsOpened);
@@ -13,9 +14,7 @@ export const PostActions = () => {
         <div className="bg-gray-600 px-0 py-2 flex items-center relative overflow-hidden">
             <div className={sharedClasses}>
                 <div className="flex items-center" onClick={handleLikeButton}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
-                    </svg>
+                    <ThumbUpIcon className="h-4 w-4 mr-1" />
                     Like
                 </div>
             </div>
@@ -25,25 +24,19 @@ export const PostActions = () => {
             />
             <div className={sharedClasses}>
                 <div className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clipRule="evenodd" />
-                    </svg>
+                    <ChatAltIcon className="h-4 w-4 mr-1" />
                     Comment
                 </div>
             </div>
             <div className={sharedClasses}>
                 <div className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-                    </svg>
+                    <RefreshIcon className="h-4 w-4 mr-1" />
                     Repost
                 </div>
             </div>
             <div className={sharedClasses}>
                 <div className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
-                    </svg>
+                    <ShareIcon className="h-4 w-4 mr-1" />
                     Share
                 </div>
             </div>
