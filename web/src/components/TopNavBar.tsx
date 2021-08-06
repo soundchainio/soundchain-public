47e838bc24abb067745f8824b4e9aedb88f2a090
Caainã Jeronimo <caainaje@gmail.com>
2021-08-06 10:05:14 -0300

    fix link errors

--- web/src/components/TopNavBar.tsx
+++ web/src/components/TopNavBar.tsx
@@ -7,6 +7,7 @@ import { Button } from './Button';
 export const TopNavBar = () => {
   const router = useRouter();
   const me = useMe();
+
   return (
     <nav className="bg-gray-20">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
