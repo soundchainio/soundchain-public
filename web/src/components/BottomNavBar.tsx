404fb90d1729b3b3c54d92a84be97d09a3b7f8ad
Jean Mayer <jeanmichaelm@outlook.com>
2021-08-06 12:41:12 -0300

    Removing unused vars

--- web/src/components/BottomNavBar.tsx
+++ web/src/components/BottomNavBar.tsx
@@ -4,14 +4,12 @@ import { Notification } from 'icons/Notification';
 import { Profile } from 'icons/Profile';
 import { Search } from 'icons/Search';
 import { setJwt } from 'lib/apollo';
-import { useRouter } from 'next/dist/client/router';
 import { useState } from 'react';
 import { NewPostModal } from './NewPostModal';
 
 export const BottomNavBar = () => {
   const [showNewPost, setShowNewPost] = useState(false);
 
-  const router = useRouter();
   return (
     <nav className="bg-gray-20 h-16 flex items-center fixed bottom-0 inset-x-0 shadow-2xl">
       <div className="w-full flex justify-around">
