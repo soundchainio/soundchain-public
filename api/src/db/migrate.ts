653aeddcbbc14b07ba4dc3aa5912e025e86d36af
Caainã Jeronimo <caainaje@gmail.com>
2021-08-13 15:16:25 -0300

    catch error

--- api/src/db/migrate.ts
+++ api/src/db/migrate.ts
@@ -2,9 +2,15 @@ import type { Handler } from 'aws-lambda';
 import { database, up } from 'migrate-mongo';
 
 export const handler: Handler = async () => {
+  let success = false;
   const { db, client } = await database.connect();
-  const migrated = await up(db);
-  migrated.forEach(fileName => console.log('Migrated:', fileName));
+  try {
+    const migrated = await up(db);
+    migrated.forEach(fileName => console.log('Migrated:', fileName));
+    success = true;
+  } catch (err) {
+    console.log(`Migration Failed: ${err.message}`);
+  }
   await client.close();
-  return true;
+  return success;
 };
