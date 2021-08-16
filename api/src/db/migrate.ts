b6a74207786ecee21d0a6973d7f55900f21a7ece
Caainã Jeronimo <caainaje@gmail.com>
2021-08-16 19:00:32 -0300

    remove return and let throw error

--- api/src/db/migrate.ts
+++ api/src/db/migrate.ts
@@ -3,17 +3,10 @@ import { database, up } from 'migrate-mongo';
 
 export const handler: Handler = async () => {
   console.log('Starting migrations');
-  let success = false;
   const { db, client } = await database.connect();
-  try {
-    const migrated = await up(db);
-    if (migrated.length) migrated.forEach(fileName => console.log('Migrated:', fileName));
-    else console.log('No pending migrations');
-    success = true;
-    console.log('Migrations finish');
-  } catch (err) {
-    console.log(`Migration Failed: ${err.message}`);
-  }
+  const migrated = await up(db);
+  if (migrated.length) migrated.forEach(fileName => console.log('Migrated:', fileName));
+  else console.log('No pending migrations');
   await client.close();
-  return success;
+  console.log('Migrations finish');
 };
