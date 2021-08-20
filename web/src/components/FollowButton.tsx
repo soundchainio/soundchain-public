f8558fc3f2e1bbf3bdda88309a70a34f5f9dc3b8
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2021-08-20 09:18:42 -0300

    Remove follow button from own profile

--- web/src/components/FollowButton.tsx
+++ web/src/components/FollowButton.tsx
@@ -1,15 +1,15 @@
-import { Button, ButtonProps } from 'components/Button';
+import { Button } from 'components/Button';
 import { useMe } from 'hooks/useMe';
 import { useFollowProfileMutation, useUnfollowProfileMutation } from 'lib/graphql';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-interface FollowButtonProps extends ButtonProps {
+interface FollowButtonProps {
   followedId: string;
   isFollowed: boolean;
 }
 
-export const FollowButton = ({ followedId, isFollowed, ...rest }: FollowButtonProps) => {
+export const FollowButton = ({ followedId, isFollowed }: FollowButtonProps) => {
   const [followProfile, { loading: followLoading }] = useFollowProfileMutation();
   const [unfollowProfile, { loading: unfollowLoading }] = useUnfollowProfileMutation();
   const router = useRouter();
@@ -34,8 +34,12 @@ export const FollowButton = ({ followedId, isFollowed, ...rest }: FollowButtonPr
     }
   };
 
+  if (me?.profile.id === followedId) {
+    return null;
+  }
+
   return (
-    <Button onClick={handleClick} variant="rainbow-rounded" className="w-20 bg-gray-10 text-sm" {...rest}>
+    <Button onClick={handleClick} variant="rainbow-rounded" className="w-20 bg-gray-10 text-sm">
       {isFollowed ? 'Unfollow' : 'Follow'}
     </Button>
   );
