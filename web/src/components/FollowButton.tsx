ddd94eb2e94fff0a541c8098a77c215485c66a48
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2021-08-20 09:08:23 -0300

    Fix follow button for guest users

--- web/src/components/FollowButton.tsx
+++ web/src/components/FollowButton.tsx
@@ -1,18 +1,30 @@
-import { Button } from 'components/Button';
+import { Button, ButtonProps } from 'components/Button';
+import { useMe } from 'hooks/useMe';
 import { useFollowProfileMutation, useUnfollowProfileMutation } from 'lib/graphql';
+import { useRouter } from 'next/router';
 import React from 'react';
 
-interface FollowButtonProps {
+interface FollowButtonProps extends ButtonProps {
   followedId: string;
   isFollowed: boolean;
 }
 
-export const FollowButton = ({ followedId, isFollowed }: FollowButtonProps) => {
+export const FollowButton = ({ followedId, isFollowed, ...rest }: FollowButtonProps) => {
   const [followProfile, { loading: followLoading }] = useFollowProfileMutation();
   const [unfollowProfile, { loading: unfollowLoading }] = useUnfollowProfileMutation();
+  const router = useRouter();
+  const me = useMe();
 
   const handleClick = async () => {
-    if (followLoading || unfollowLoading) return;
+    if (followLoading || unfollowLoading) {
+      return;
+    }
+
+    if (!me) {
+      router.push('/login', { query: { redirect_url: window.location.href } });
+      return;
+    }
+
     const opts = { variables: { input: { followedId } } };
 
     if (!isFollowed) {
@@ -23,7 +35,7 @@ export const FollowButton = ({ followedId, isFollowed }: FollowButtonProps) => {
   };
 
   return (
-    <Button onClick={handleClick} variant="rainbow-rounded" className="w-20 bg-gray-10 text-sm">
+    <Button onClick={handleClick} variant="rainbow-rounded" className="w-20 bg-gray-10 text-sm" {...rest}>
       {isFollowed ? 'Unfollow' : 'Follow'}
     </Button>
   );
