095214c67b519e33095e21523953fa89c890205e
Caainã Jeronimo <caainaje@gmail.com>
2021-09-01 16:04:30 -0300

    refactor chat

--- web/src/components/Chat.tsx
+++ web/src/components/Chat.tsx
@@ -1,9 +1,8 @@
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 import { Message as MessageItem, useChatHistoryQuery } from 'lib/graphql';
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroller';
+import { useEffect, useRef, useState } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
-import { LoaderAnimation } from './LoaderAnimation';
+import { InfiniteLoader } from './InfiniteLoader';
 import { Message } from './Message';
 import { MessageSkeleton } from './MessageSkeleton';
 
@@ -13,14 +12,26 @@ interface ChatProps {
 
 export const Chat = ({ profileId }: ChatProps) => {
   const [renderLoader, setRenderLoader] = useState(false);
+  const [lastContainerHeight, setLastContainerHeight] = useState(0);
   const containerRef = useRef<HTMLDivElement>(null);
   const { data, fetchMore } = useChatHistoryQuery({
     variables: { profileId },
   });
 
-  useLayoutEffect(() => {
-    if (containerRef.current) console.log(containerRef.current.scrollHeight);
-  }, [containerRef.current?.scrollHeight]);
+  useEffect(() => {
+    if (!containerRef.current) return;
+    if (lastContainerHeight !== containerRef.current?.scrollHeight) {
+      requestAnimationFrame(() => {
+        if (!containerRef.current) return;
+        disableBodyScroll(containerRef.current);
+        scroll.scrollTo(containerRef.current?.scrollHeight - lastContainerHeight, { duration: 0 });
+        requestAnimationFrame(() => {
+          if (containerRef.current) enableBodyScroll(containerRef.current);
+        });
+      });
+    }
+    setLastContainerHeight(containerRef.current?.scrollHeight);
+  }, [containerRef.current?.scrollHeight, lastContainerHeight]);
 
   useEffect(() => {
     if (!renderLoader && data) initialScrollToBottom();
@@ -31,31 +42,24 @@ export const Chat = ({ profileId }: ChatProps) => {
   const { nodes: messages, pageInfo } = data.chatHistory;
 
   const initialScrollToBottom = () => {
-    setTimeout(() => scroll.scrollToBottom({ duration: 0 }), 100);
-    setTimeout(() => setRenderLoader(true), 2000);
+    requestAnimationFrame(() => {
+      setTimeout(() => scroll.scrollToBottom({ duration: 0 }), 100);
+      setTimeout(() => setRenderLoader(true), 1000);
+    });
   };
 
   const loadMore = async () => {
-    if (containerRef.current) disableBodyScroll(containerRef.current);
     await fetchMore({ variables: { profileId, page: { after: pageInfo.startCursor } } });
-    if (containerRef.current) enableBodyScroll(containerRef.current);
   };
 
   return (
-    <InfiniteScroll
-      loadMore={loadMore}
-      hasMore={renderLoader && pageInfo.hasNextPage}
-      loader={
-        <div className="flex items-center">
-          <LoaderAnimation loadingMessage="Loading messages" />
-        </div>
-      }
-      isReverse
-      className="flex flex-col m-3 space-y-4"
-    >
-      {messages.map(({ id }, index) => (
-        <Message key={id} messageId={id} nextMessage={messages[index + 1] as MessageItem} />
-      ))}
-    </InfiniteScroll>
+    <div ref={containerRef}>
+      {renderLoader && pageInfo.hasNextPage && <InfiniteLoader loadMore={loadMore} loadingMessage="Loading messages" />}
+      <div className="flex flex-col m-3 space-y-4">
+        {messages.map(({ id }, index) => (
+          <Message key={id} messageId={id} nextMessage={messages[index + 1] as MessageItem} />
+        ))}
+      </div>
+    </div>
   );
 };
