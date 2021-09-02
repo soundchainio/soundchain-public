b2eeedc2c09249520442bdaff72b4a3e78135cdf
Mason Seale <mason.seale@ae.studio>
2021-09-02 15:58:50 -0700

    reaction selector updates

--- web/src/components/ReactionSelector.tsx
+++ web/src/components/ReactionSelector.tsx
@@ -1,14 +1,20 @@
+import classNames from 'classnames';
 import { ReactionEmoji } from 'icons/ReactionEmoji';
-import { ReactionType, useReactToPostMutation } from 'lib/graphql';
+import {
+  ReactionType,
+  useChangeReactionMutation,
+  useReactToPostMutation,
+  useRetractReactionMutation,
+} from 'lib/graphql';
 import React from 'react';
 
 interface ReactionSelectorProps {
   postId: string;
+  myReaction: ReactionType | null;
   opened: boolean;
   setOpened: (val: boolean) => void;
 }
 
-const baseClasses = 'list-none flex absolute duration-500 ease-in-out bg-gray-25 transform-gpu transform w-3/4 right-0';
 const reactionTypes = [
   ReactionType.Heart,
   ReactionType.Horns,
@@ -17,21 +23,40 @@ const reactionTypes = [
   ReactionType.Sunglasses,
 ];
 
-export const ReactionSelector = ({ postId, opened, setOpened }: ReactionSelectorProps) => {
+const baseListClasses =
+  'list-none flex absolute right-0 duration-500 ease-in-out bg-gray-25 transform-gpu transform w-3/4';
+
+export const ReactionSelector = ({ postId, myReaction, opened, setOpened }: ReactionSelectorProps) => {
   const [reactToPost] = useReactToPostMutation();
+  const [changeReaction] = useChangeReactionMutation();
+  const [retractReaction] = useRetractReactionMutation();
 
   const handleSelect = async (type: ReactionType) => {
-    await reactToPost({ variables: { input: { postId, type } } });
+    if (type === myReaction) {
+      await retractReaction({ variables: { input: { postId } } });
+    } else if (myReaction) {
+      await changeReaction({ variables: { input: { postId, type } } });
+    } else {
+      await reactToPost({ variables: { input: { postId, type } } });
+    }
+
     setOpened(false);
   };
 
   const ListOptions = reactionTypes.map(reaction => {
     return (
-      <li key={reaction} className="flex-1 text-center cursor-pointer" onClick={() => handleSelect(reaction)}>
-        <ReactionEmoji name={reaction} className="w-5" />
+      <li key={reaction} className="flex-1 flex justify-center cursor-pointer" onClick={() => handleSelect(reaction)}>
+        <div
+          className={classNames('rounded-lg px-3 py-2', {
+            'bg-gray-40': myReaction === reaction,
+            'opacity-50': myReaction && myReaction !== reaction,
+          })}
+        >
+          <ReactionEmoji name={reaction} className="w-5 h-5" />
+        </div>
       </li>
     );
   });
 
-  return <ul className={`${baseClasses} ${opened ? 'translate-x-4/4' : 'translate-x-full'}`}>{ListOptions}</ul>;
+  return <ul className={`${baseListClasses} ${opened ? 'translate-x-4/4' : 'translate-x-full'}`}>{ListOptions}</ul>;
 };
