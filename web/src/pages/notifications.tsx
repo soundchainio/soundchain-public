d3f8a15f04a23da5accd20afeff3ba8b120b944b
Mortaro <christian.mortaro@gmail.com>
2021-09-06 18:52:28 -0300

    :sparkles: protect notifications

--- web/src/pages/notifications.tsx
+++ web/src/pages/notifications.tsx
@@ -2,14 +2,19 @@ import { ClearAllNotificationsButton } from 'components/ClearAllNotificationsBut
 import { Layout } from 'components/Layout';
 import { Notifications } from 'components/Notifications';
 import { TopNavBarProps } from 'components/TopNavBar';
+import { useMe } from 'hooks/useMe';
 import { useResetNotificationCountMutation } from 'lib/graphql';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 export default function UserNotifications() {
   const [resetNotificationCount] = useResetNotificationCountMutation();
+  const me = useMe()
+  const router = useRouter()
 
   useEffect(() => {
+    if(!me) return router.push('/login')
     resetNotificationCount();
   }, []) 
 
