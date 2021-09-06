002aea5a68c8ad56da73d1653eea7341cb38dd6d
Mortaro <christian.mortaro@gmail.com>
2021-09-06 17:21:08 -0300

    :sparkles: load with pages with skeleton

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,13 +2,7 @@ import { Feed } from 'components/Feed';
 import { Layout } from 'components/Layout';
 import { Posts } from 'components/Posts';
 import { useMe } from 'hooks/useMe';
-import { cacheFor } from 'lib/apollo';
-import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-
-export const getServerSideProps: GetServerSideProps = context => {
-  return cacheFor(HomePage, {}, context);
-};
 
 export default function HomePage() {
   const me = useMe();
--- web/src/pages/notifications.tsx
+++ web/src/pages/notifications.tsx
@@ -2,15 +2,7 @@ import { ClearAllNotificationsButton } from 'components/ClearAllNotificationsBut
 import { Layout } from 'components/Layout';
 import { Notifications } from 'components/Notifications';
 import { TopNavBarProps } from 'components/TopNavBar';
-import { cacheFor } from 'lib/apollo';
-import { ResetNotificationCountDocument, ResetNotificationCountMutation } from 'lib/graphql';
-import { protectPage } from 'lib/protectPage';
 import Head from 'next/head';
-
-export const getServerSideProps = protectPage(async (context, apolloClient) => {
-  await apolloClient.mutate<ResetNotificationCountMutation>({ mutation: ResetNotificationCountDocument, context });
-  return cacheFor(UserNotifications, {}, context, apolloClient);
-});
 
 export default function UserNotifications() {
   const topNavBarProps: TopNavBarProps = {
