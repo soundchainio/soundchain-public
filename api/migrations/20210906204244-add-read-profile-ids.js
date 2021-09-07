e3ff2817170742ada504bbffb623e79138acb042
Caainã Jeronimo <caainaje@gmail.com>
2021-09-07 16:11:55 -0300

    remove migration

--- api/migrations/20210906204244-add-read-profile-ids.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  async up(db) {
-    await db.collection('messages').updateMany({}, { $set: { readProfileIds: [] } });
-  },
-
-  async down(db) {
-    await db.collection('messages').updateMany({}, { $unset: { readProfileIds: '' } });
-  },
-};
