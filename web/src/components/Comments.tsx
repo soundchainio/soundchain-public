a06231ef06870c853b1d3f469acce68b3993980b
Mason Seale <mason.seale@ae.studio>
2021-09-08 14:44:58 -0700

    troubleshoot comments rendering

--- web/src/components/Comments.tsx
+++ web/src/components/Comments.tsx
@@ -1,7 +1,5 @@
-import { ApolloQueryResult } from '@apollo/client';
-import { CommentsQuery, PageInfo, PageInput, useCommentsQuery } from 'lib/graphql';
+import { PageInput, useCommentsQuery } from 'lib/graphql';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 import { Comment } from './Comment';
 import { CommentSkeleton } from './CommentSkeleton';
 import { InfiniteLoader } from './InfiniteLoader';
@@ -19,61 +17,42 @@ export const Comments = ({ postId, pageSize = 10 }: CommentsProps) => {
     firstPage.after = inclusiveCursor;
     firstPage.inclusive = true;
   }
-  const [newestPageInfo, setNewestPageInfo] = useState<PageInfo | null>(null);
-  const [oldestPageInfo, setOldestPageInfo] = useState<PageInfo | null>(null);
 
   const { data, fetchMore } = useCommentsQuery({ variables: { postId, page: firstPage } });
 
-  useEffect(() => {
-    if (!newestPageInfo) {
-      setNewestPageInfo(data?.comments.pageInfo);
-    }
-    if (!oldestPageInfo) {
-      setOldestPageInfo(data?.comments.pageInfo);
-    }
-  }, [data?.comments.pageInfo]);
-
   if (!data) return <CommentSkeleton />;
 
-  const { nodes: comments } = data.comments;
-
-  const loadNext = async () => {
-    const { data }: ApolloQueryResult<CommentsQuery> = await fetchMore({
+  const { nodes: comments, pageInfo } = data.comments;
+  console.log(comments.length);
+  const loadNext = () => {
+    console.log('LOAD NEXT');
+    fetchMore({
       variables: {
         page: {
           first: pageSize,
-          after: oldestPageInfo?.endCursor,
+          after: pageInfo.endCursor,
           inclusive: false,
         },
       },
     });
-
-    if (data.comments.pageInfo) {
-      setOldestPageInfo(data.comments.pageInfo as PageInfo);
-    }
   };
 
-  const loadPrevious = async () => {
-    const { data }: ApolloQueryResult<CommentsQuery> = await fetchMore({
+  const loadPrevious = () => {
+    fetchMore({
       variables: {
         page: {
           last: pageSize,
-          before: newestPageInfo?.startCursor,
+          before: pageInfo.startCursor,
           inclusive: false,
         },
       },
     });
-
-    if (data.comments.pageInfo) {
-      console.log('NEWEST PAGE', data.comments.pageInfo);
-      setNewestPageInfo(data.comments.pageInfo as PageInfo);
-    }
   };
 
   return (
     <div className="flex flex-col m-4 space-y-4">
       <h3 className="font-thin text-white">Comments</h3>
-      {newestPageInfo?.hasPreviousPage && (
+      {pageInfo.hasPreviousPage && (
         <div onClick={loadPrevious} className="cursor-pointer text-white">
           View Newer Comments
         </div>
@@ -81,7 +60,7 @@ export const Comments = ({ postId, pageSize = 10 }: CommentsProps) => {
       {comments.map(({ id }) => (
         <Comment key={id} commentId={id} />
       ))}
-      {oldestPageInfo?.hasNextPage && <InfiniteLoader loadMore={loadNext} loadingMessage="Loading Comments" />}
+      {pageInfo.hasNextPage && <InfiniteLoader loadMore={loadNext} loadingMessage="Loading Comments" />}
     </div>
   );
 };
--- web/src/lib/apollo/cache-config.ts
+++ web/src/lib/apollo/cache-config.ts
@@ -1,5 +1,5 @@
 import { InMemoryCacheConfig } from '@apollo/client';
-import { Comment, CommentConnection, FeedConnection, FeedItem } from 'lib/graphql';
+import { FeedConnection, FeedItem } from 'lib/graphql';
 
 export const cacheConfig: InMemoryCacheConfig = {
   typePolicies: {
@@ -40,49 +40,59 @@ export const cacheConfig: InMemoryCacheConfig = {
         },
         comments: {
           keyArgs: ['postId'],
-          // merge(existing = { nodes: [] }, { pageInfo, nodes }, { args, readField }) {
-          //   const existingNodeIds = existing.nodes.map(node => readField('id', node));
-          //   const newNodes = nodes.filter(node => !existingNodeIds.includes(readField('id', node)));
-          //   if (args?.page.before) {
-          //     return {
-          //       nodes: [...newNodes, ...existing.nodes],
-          //       pageInfo,
-          //     };
-          //   }
-          //   return {
-          //     nodes: [...existing.nodes, ...newNodes],
-          //     pageInfo,
-          //   };
-          // },
-          merge(existing, incoming, { readField }): CommentConnection {
-            const nodes = existing ? { ...existing.nodes } : {};
+          merge(existing = { nodes: [] }, { pageInfo, nodes }, { args, readField }) {
+            // const existingNodeIds = existing.nodes.map(node => readField('id', node));
+            // const newNodes = nodes.filter(node => !existingNodeIds.includes(readField('id', node)));
 
-            incoming.nodes.forEach((node: Comment) => {
-              const key = readField('id', node);
-              nodes[key as string] = node;
-            });
-
-            return {
-              pageInfo: {
-                ...incoming.pageInfo,
-              },
-              nodes,
-            };
-          },
-          read(existing, { readField }): CommentConnection | void {
-            if (existing) {
-              const nodes = Object.values(existing.nodes);
-              const newestComments = nodes.sort((a, b) => {
-                const aDate = new Date(readField('createdAt', a));
-                const bDate = new Date(readField('createdAt', b));
-                return bDate - aDate;
-              });
+            if (args?.page.before) {
               return {
-                pageInfo: { ...existing.pageInfo },
-                nodes: newestComments,
+                nodes: [...nodes, ...existing.nodes],
+                pageInfo: {
+                  ...existing.pageInfo,
+                  startCursor: pageInfo.startCursor,
+                  hasPreviousPage: pageInfo.hasPreviousPage,
+                },
               };
             }
+            console.log('MERGING');
+            return {
+              nodes: [...existing.nodes, ...nodes],
+              pageInfo: {
+                ...existing.pageInfo,
+                endCursor: pageInfo.endCursor,
+                hasNextPage: pageInfo.hasNextPage,
+              },
+            };
           },
+          // merge(existing, incoming, { readField }): CommentConnection {
+          //   const nodes = existing ? { ...existing.nodes } : {};
+
+          //   incoming.nodes.forEach((node: Comment) => {
+          //     const key = readField('id', node);
+          //     nodes[key as string] = node;
+          //   });
+
+          //   return {
+          //     pageInfo: {
+          //       ...incoming.pageInfo,
+          //     },
+          //     nodes,
+          //   };
+          // },
+          // read(existing, { readField }): CommentConnection | void {
+          //   if (existing) {
+          //     const nodes = Object.values(existing.nodes);
+          //     const newestComments = nodes.sort((a, b) => {
+          //       const aDate = new Date(readField('createdAt', a));
+          //       const bDate = new Date(readField('createdAt', b));
+          //       return bDate - aDate;
+          //     });
+          //     return {
+          //       pageInfo: { ...existing.pageInfo },
+          //       nodes: newestComments,
+          //     };
+          //   }
+          // },
         },
         feed: {
           keyArgs: false,
