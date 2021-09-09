bafc4be71dc4283ce368b105299ca48be22e2807
Caainã Jeronimo <caainaje@gmail.com>
2021-09-09 19:57:11 -0300

    fix chat elipses

--- web/src/components/ChatItem.tsx
+++ web/src/components/ChatItem.tsx
@@ -22,21 +22,20 @@ export const ChatItem = ({
   return (
     <NextLink href={`/messages/${id}`}>
       <div className={classNames('flex flex-col py-4 pl-1 pr-4', unread ? 'odd:bg-gray-25 bg-gray-20' : 'bg-gray-15')}>
-        <div className="break-words flex items-center">
-          <div className="w-2 self-center items-center mr-2">
+        <div className="flex items-center flex-row">
+          <div className="flex w-2 self-center items-center mr-2">
             <div className={classNames('w-[6px] h-[6px] rounded-full', unread && 'bg-purple-gradient')}></div>
           </div>
-          <div className="flex pr-4">
-            <Avatar className="flex" profile={{ profilePicture: profilePicture }} pixels={40} />
-          </div>
-          <div>
-            <div className="font-semibold text-white flex text-sm">{displayName}</div>
-            <div className="text-gray-80 flex text-sm">
-              {`${message}`}&nbsp;<div className="text-gray-40">•</div>&nbsp;
+          <Avatar className="flex w-[40px]" profile={{ profilePicture: profilePicture }} pixels={40} />
+          <div className="flex w-9/12 flex-col pl-4">
+            <div className="font-semibold text-white flex text-sm line-clamp-1">{displayName}</div>
+            <div className="text-gray-80 flex text-sm whitespace-nowrap">
+              <div className="truncate">{`${message}`}</div>&nbsp;<div className="text-gray-40">•</div>
+              &nbsp;
               <Timestamp small datetime={createdAt} className="text-sm" />
             </div>
           </div>
-          <div className="items-end ml-auto mt-auto mb-auto">
+          <div className="flex w-2/12 justify-end">
             <CircleRightArrow />
           </div>
         </div>
