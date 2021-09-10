c26155601b4d49b06a94dafb0e485b912b7a5d14
Jean Michael Mayer <jeanmichaelm@outlook.com>
2021-09-10 00:21:20 -0300

    feature/reaction-list

--- web/src/components/ReactionItem.tsx
+++ web/src/components/ReactionItem.tsx
@@ -1,12 +1,14 @@
-import { Reaction } from 'lib/graphql';
+import { Reaction, ReactionType } from 'lib/graphql';
 import Link from 'next/link';
 import { Avatar } from './Avatar';
+import { ReactionEmoji } from 'icons/ReactionEmoji';
 
 interface FollowItemProps {
   reaction: Reaction;
 }
 
 export const ReactionItem = ({ reaction: { type, profile } }: FollowItemProps) => {
+  console.log('type: ', type);
   return (
     <>
       <Link href={`/profiles/${profile.id}`} passHref>
@@ -14,8 +16,13 @@ export const ReactionItem = ({ reaction: { type, profile } }: FollowItemProps) =
           <div className="items-center self-center content-center">
             <Avatar pixels={40} className="flex" profile={profile} />
           </div>
-          <div className="text-white font-bold">
-            {profile.displayName} {type}
+          <div className="text-white font-bold flex w-full">
+            <div className="flex-1">
+              {profile.displayName}
+            </div>
+            <div className="w-16 text-center">
+              {/* <ReactionEmoji name={ReactionType[`${type}`] as ReactionType} className="w-4 h-4" /> */}
+            </div>
           </div>
         </div>
       </Link>
