862043c88fb7589e1270662e51908cfa2b13e1b9
Mason Seale <mason.seale@ae.studio>
2021-09-13 11:37:56 -0700

    refactor auth layout

--- web/src/components/Buttons/GreenGradient.tsx
+++ web/src/components/Buttons/GreenGradient.tsx
@@ -4,9 +4,13 @@ import { ButtonProps, commonClasses } from 'components/Button';
 export const GreenGradient = ({ className, type = 'button', icon: Icon, children, ...rest }: ButtonProps) => {
   return (
     <div>
-      <button className={classNames(commonClasses, 'sm:px-4 py-1 font-bold', className)} type={type} {...rest}>
+      <button
+        className={classNames(commonClasses, 'sm:px-4 py-1 font-bold uppercase', className)}
+        type={type}
+        {...rest}
+      >
         {Icon && <Icon className="mr-1 h-5 w-5" />}
-        <span className="capitalize green-gradient-text">{children}</span>
+        <span className="green-gradient-text">{children}</span>
       </button>
     </div>
   );
