a6eff215b83bfc7fa3c166d63fb48fc7a89db24e
Caainã Jeronimo <caainaje@gmail.com>
2021-09-15 08:21:02 -0300

    create profile picture page

--- web/src/components/ImageUpload.tsx
+++ web/src/components/ImageUpload.tsx
@@ -1,16 +1,14 @@
 import classNames from 'classnames';
 import { useUpload } from 'hooks/useUpload';
-import { Camera } from 'icons/Camera';
+import { Upload } from 'icons/Upload';
 import Image from 'next/image';
 import Dropzone from 'react-dropzone';
-import { Label } from './Label';
 
 export interface ImageUploadProps extends Omit<React.ComponentPropsWithoutRef<'div'>, 'onChange'> {
   maxNumberOfFiles?: number;
   maxFileSize?: number;
   value?: string;
   onChange(value: string): void;
-  roundedFull?: boolean;
 }
 
 const defaultMaxFileSize = 1024 * 1024 * 3; // 3Mb
@@ -22,7 +20,6 @@ export function ImageUpload({
   maxFileSize = defaultMaxFileSize,
   value,
   onChange,
-  roundedFull,
   children,
   ...rest
 }: ImageUploadProps) {
@@ -41,8 +38,8 @@ export function ImageUpload({
         <div
           className={classNames(
             className,
-            'relative flex items-center justify-center bg-gray-30',
-            roundedFull ? 'rounded-full' : 'rounded-lg',
+            'relative flex items-center justify-center bg-gray-30 border-gray-80 border-2',
+            thumbnail ? 'w-16 h-16 rounded-full' : 'w-3/4 rounded-lg',
           )}
           {...rest}
           {...getRootProps()}
@@ -50,16 +47,16 @@ export function ImageUpload({
           <input {...getInputProps()} />
           {thumbnail ? (
             <Image
-              className={classNames('object-cover', roundedFull ? 'rounded-full' : 'rounded-lg')}
+              className={classNames('object-cover rounded-full')}
               src={thumbnail}
               alt="Upload preview"
               layout="fill"
             />
           ) : (
-            <>
-              <Label className="p-4 text-center">{children}</Label>
-              <Camera className="absolute -bottom-2 -right-2" />
-            </>
+            <div className="flex flex-row p-4 text-center text-white text-sm font-semibold">
+              <Upload className="mr-2 mt-[2px]" />
+              {children}
+            </div>
           )}
         </div>
       )}
---
+++ web/src/pages/settings/profile-picture.tsx
@@ -0,0 +1,69 @@
+import { Button } from 'components/Button';
+import { BackButton } from 'components/Buttons/BackButton';
+import { ImageUploadField } from 'components/ImageUploadField';
+import { InputField } from 'components/InputField';
+import { Label } from 'components/Label';
+import { Layout } from 'components/Layout';
+import { TopNavBarProps } from 'components/TopNavBar';
+import { Form, Formik } from 'formik';
+import { useMe } from 'hooks/useMe';
+import { useUpdateProfileDisplayNameMutation, useUpdateProfilePictureMutation } from 'lib/graphql';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import * as yup from 'yup';
+
+export interface ProfilePictureFormValues {
+  profilePicture?: string | undefined;
+}
+
+const validationSchema: yup.SchemaOf<ProfilePictureFormValues> = yup.object().shape({
+  profilePicture: yup.string(),
+});
+
+const topNavBarProps: TopNavBarProps = {
+  title: 'Profile Picture',
+  leftButton: <BackButton />,
+};
+
+export default function ProfilePicturePage() {
+  const me = useMe();
+  const router = useRouter();
+  const fromSettings = Boolean(router.query.fromSettings);
+  const initialFormValues: ProfilePictureFormValues = { profilePicture: '' };
+  const [updateProfilePicture, { loading }] = useUpdateProfilePictureMutation();
+
+  const onSubmit = async ({ profilePicture }: ProfilePictureFormValues) => {
+    await updateProfilePicture({ variables: { input: { picture: profilePicture as string } } });
+    router.push('/settings');
+  };
+
+  if (!me) return null;
+
+  return (
+    <Layout topNavBarProps={topNavBarProps} hideBottomNavBar>
+      <Head>
+        <title>Soundchain - Name Settings</title>
+        <meta name="description" content="Name Settings" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="min-h-full flex flex-col px-6 lg:px-8 bg-gray-20 py-6">
+        <Formik initialValues={initialFormValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+          <Form className="flex flex-1 flex-col space-y-6">
+            <div className="flex-grow">
+              <Label textSize="base">Custom Profile Photo:</Label>
+              <ImageUploadField name="profilePicture" className="mt-4">
+                Upload Profile Photo
+              </ImageUploadField>
+            </div>
+            <div className="flex flex-col ">
+              <Button type="submit" loading={loading} disabled={loading} variant="green-gradient">
+                {fromSettings ? 'Next' : 'Save'}
+              </Button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </Layout>
+  );
+}
