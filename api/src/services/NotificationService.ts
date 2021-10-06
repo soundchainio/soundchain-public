de6a27792b17250da3a268afb3763c5ac1a00af0
Jean Michael Mayer <jeanmichaelm@outlook.com>
2021-10-06 02:19:33 -0300

    Fix notification counter

--- api/src/services/NotificationService.ts
+++ api/src/services/NotificationService.ts
@@ -130,8 +130,10 @@ export class NotificationService extends ModelService<typeof Notification> {
       trackId: post.trackId,
     };
     const notifications = subscribersIds.map(
-      profileId => new this.model({ type: NotificationType.NewPost, profileId, metadata }),
+      profileId => new this.model({ type: NotificationType.NewPost, profileId, metadata })
     );
+    console.log('subscribers ids:', subscribersIds);
+    await ProfileModel.updateMany({ _id: { $in: subscribersIds } }, { $inc: { unreadNotificationCount: 1 } });
     await this.model.insertMany(notifications);
   }
 }
