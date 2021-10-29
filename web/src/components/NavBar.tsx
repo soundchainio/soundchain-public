3244e2ed362c9e038f64149e949f89f3010e03cd
Jean Mayer <jean.mayer@ae.studio>
2021-10-29 05:18:08 -0300

    Fix bottom nav colors

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -10,7 +10,6 @@ import { Search } from 'icons/Search';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { NavBarButton } from './Buttons/NavBarButton';
-import { NavBarLink } from './Buttons/NavBarLink';
 import { NotificationBadge } from './NotificationBadge';
 
 export const NavBar = () => {
@@ -26,8 +25,8 @@ export const NavBar = () => {
   return (
     <nav className="bg-black md:bg-gray-30 h-16 flex items-center inset-x-0 shadow-2xl md:w-full">
       <div className="w-full flex justify-center gap-14">
-        <NavBarLink label="Home" path="/" icon={Home} color="yellow" id="top" />
-        <NavBarLink
+        <NavBarButton label="Home" path="/" icon={Home} color="yellow" id="top" />
+        <NavBarButton
           label="Explore"
           path={me ? '/explore' : '/login'}
           icon={Search}
@@ -38,20 +37,20 @@ export const NavBar = () => {
           :
           <NavBarButton label="Create" icon={NewPost} onClick={handleCreateClick} />
         }
-        <NavBarLink
+        <NavBarButton
           label="Notifications"
           path={me ? '/notifications' : '/login'}
           icon={Bell}
           badge={me ? NotificationBadge : undefined}
           color="purple"
         />
-        <NavBarLink
+        <NavBarButton
           label="Profile"
           icon={Profile}
           path={me ? `/profiles/${me?.profile.id}` : '/login'}
           color="purple-green"
         />
-        <NavBarLink
+        <NavBarButton
           label="Playground"
           path="/playground"
           icon={() => <Happy width={20} height={20} />}
