4d7277d87fe6eeda9b2147449f47c9de62594e69
Fernando Goncalves <fernando.hmg@gmail.com>
2021-11-05 14:10:55 -0300

    Adding className prop to DisplayName component

--- web/src/components/DisplayName.tsx
+++ web/src/components/DisplayName.tsx
@@ -1,14 +1,16 @@
+import classNames from 'classnames';
 import { Verified } from 'icons/Verified';
 
 interface DisplayNameProps {
   name: string;
   verified?: boolean | null;
+  className?: string;
 }
 
-export const DisplayName = ({ name, verified, ...props }: DisplayNameProps) => {
+export const DisplayName = ({ name, verified, className, ...props }: DisplayNameProps) => {
   return (
-    <div {...props} className="text-white font-semibold flex items-center">
-      {name} {verified && <Verified className="ml-2" />}
+    <div {...props} className={classNames('text-white font-semibold flex gap-2 items-center ', className)}>
+      {name} {verified && <Verified />}
     </div>
   );
 };
