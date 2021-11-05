d89353b37ce0be1a98a358a07cfdb9c732e5a85c
Fernando Goncalves <fernando.hmg@gmail.com>
2021-11-05 02:41:23 -0300

    Replacing anchor tags on NavBar

--- web/src/components/Buttons/NavBarButton.tsx
+++ web/src/components/Buttons/NavBarButton.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import { SVGGradientColor } from 'icons/gradients';
 import { IconProps } from 'icons/types/IconProps';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
@@ -16,7 +17,16 @@ interface NavBarButtonProps {
   nyanCat?: boolean;
 }
 
-export const NavBarButton = ({ label, path, icon: Icon, onClick, badge: Badge, color, id, nyanCat }: NavBarButtonProps) => {
+export const NavBarButton = ({
+  label,
+  path,
+  icon: Icon,
+  onClick,
+  badge: Badge,
+  color,
+  id,
+  nyanCat,
+}: NavBarButtonProps) => {
   const [isActive, setActive] = useState(false);
   const router = useRouter();
 
@@ -30,34 +40,53 @@ export const NavBarButton = ({ label, path, icon: Icon, onClick, badge: Badge, c
 
   const onButtonClick = () => {
     setActive(true);
-
     if (onClick) {
       onClick();
     }
-    if (path) {
-      router.push(path);
-    }
   };
 
+  const MenuContentItem = () => {
+    return (
+      <>
+        {Icon && (
+          <div className="relative">
+            {Badge && <Badge />}
+            <Icon color={isActive ? color : undefined} id={id} />
+          </div>
+        )}
+        {nyanCat && (
+          <div className="relative overflow-hidden scale-150">
+            <Image height={20} width={40} src="/nyan-cat-cropped.gif" alt="Loading" priority />
+          </div>
+        )}
+        <span
+          className={classNames(
+            'text-gray-50 text-xs font-semibold',
+            !nyanCat && 'mt-2',
+            isActive && `${color}-gradient-text`,
+          )}
+        >
+          {label}
+        </span>
+      </>
+    );
+  };
+  const className =
+    'flex flex-col flex-1 md:flex-none items-center justify-center align-middle cursor-pointer focus:ring-2 focus:ring-blue-600';
+
+  if (path) {
+    return (
+      <Link href={path} passHref>
+        <a className={className}>
+          <MenuContentItem />
+        </a>
+      </Link>
+    );
+  }
+
   return (
-    <button
-      onClick={onButtonClick}
-      className="flex flex-col flex-1 md:flex-none items-center justify-center align-middle cursor-pointer focus:ring-2 focus:ring-blue-600"
-    >
-      {Icon && (
-        <div className="relative">
-          {Badge && <Badge />}
-          <Icon color={isActive ? color : undefined} id={id} />
-        </div>
-      )}
-      {nyanCat &&
-        <div className="relative overflow-hidden scale-150">
-          <Image height={20} width={40} src="/nyan-cat-cropped.gif" alt="Loading" priority />
-        </div>
-      }
-      <span className={classNames('text-gray-50 text-xs font-semibold', !nyanCat && 'mt-2', isActive && `${color}-gradient-text`)}>
-        {label}
-      </span>
+    <button onClick={onButtonClick} className={className}>
+      <MenuContentItem />
     </button>
   );
 };
