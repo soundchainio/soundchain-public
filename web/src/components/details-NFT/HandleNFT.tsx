b0b581c1a4e9759bc4c110d6ece84c3c4b54b185
Vinicius Macelai <vinicius.macelai@gmail.com>
2021-11-08 13:50:23 -0300

    feat: frontend verification to not be able to mint without verification

--- web/src/components/details-NFT/HandleNFT.tsx
+++ web/src/components/details-NFT/HandleNFT.tsx
@@ -2,15 +2,18 @@ import { Button } from 'components/Button';
 import { useMe } from 'hooks/useMe';
 import { CheckmarkFilled } from 'icons/CheckmarkFilled';
 import { Matic } from 'icons/Matic';
+import { XMarkFilled } from 'icons/XMark';
 import { useRouter } from 'next/router';
+import React from 'react';
 
 interface HandleNFTProps {
   isOwner: boolean;
   isForSale: boolean;
+  canList: boolean;
   price: string | undefined;
 }
 
-export const HandleNFT = ({ isOwner, isForSale, price }: HandleNFTProps) => {
+export const HandleNFT = ({ isOwner, isForSale, price, canList }: HandleNFTProps) => {
   const router = useRouter();
   const me = useMe();
 
@@ -35,20 +38,6 @@ export const HandleNFT = ({ isOwner, isForSale, price }: HandleNFTProps) => {
         </div>
       </div>
     );
-  } else if (isOwner) {
-    return (
-      <div className="w-full bg-black text-white flex items-center py-3">
-        <div className="flex items-center flex-1 gap-2 text-sm font-bold pl-4">
-          <CheckmarkFilled />
-          You own this NFT
-        </div>
-        <div className="flex-1 flex items-center justify-center">
-          <Button variant="list-nft" onClick={handleListButton}>
-            <div className="px-4 font-bold">LIST NFT </div>
-          </Button>
-        </div>
-      </div>
-    );
   } else if (isForSale && price) {
     return (
       <div className="w-full bg-black text-white flex items-center py-3">
@@ -62,6 +51,29 @@ export const HandleNFT = ({ isOwner, isForSale, price }: HandleNFTProps) => {
         <div className="flex-1 flex items-center justify-center">
           <Button variant="buy-nft" onClick={() => router.push(`${router.asPath}/buy`)}>
             BUY NFT
+          </Button>
+        </div>
+      </div>
+    );
+  } else if (!canList) {
+    return (
+      <div className="w-full bg-black text-white flex justify-center items-center py-3">
+        <div className="flex items-center gap-2 text-sm font-bold">
+          <XMarkFilled />
+          You have to be verified to list your NFT
+        </div>
+      </div>
+    );
+  } else if (isOwner) {
+    return (
+      <div className="w-full bg-black text-white flex items-center py-3">
+        <div className="flex items-center flex-1 gap-2 text-sm font-bold pl-4">
+          <CheckmarkFilled />
+          You own this NFT
+        </div>
+        <div className="flex-1 flex items-center justify-center">
+          <Button variant="list-nft" onClick={handleListButton}>
+            <div className="px-4 font-bold">LIST NFT </div>
           </Button>
         </div>
       </div>
