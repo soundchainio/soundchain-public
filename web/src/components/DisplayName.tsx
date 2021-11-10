1bce0c70eb8bf5197cf7fdae73895b27413dd531
Mateus Batista <mateusalfredobatista@gmail.com>
2021-11-10 10:25:32 -0300

    profile fixes

--- web/src/components/DisplayName.tsx
+++ web/src/components/DisplayName.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import { Verified } from 'icons/Verified';
+import { forwardRef } from 'react';
 
 interface DisplayNameProps {
   name: string;
@@ -7,13 +8,16 @@ interface DisplayNameProps {
   className?: string;
 }
 
-export const DisplayName = ({ name, verified, className, ...props }: DisplayNameProps) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const DisplayName = forwardRef<any, DisplayNameProps>(({ name, verified, className, ...props }, ref) => {
   return (
-    <div className={classNames('flex gap-1 items-baseline', className)}>
+    <div className={classNames('flex gap-1 items-baseline', className)} ref={ref}>
       <span {...props} className="text-white font-semibold truncate">
         {name}
       </span>
       {verified && <Verified aria-label="Verified user" className="flex-shrink-0" />}
     </div>
   );
-};
+});
+
+DisplayName.displayName = 'DisplayName';
