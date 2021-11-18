a8e10bd081424f664d57defff2090aa9e7987616
Vinicius Macelai <vinicius.macelai@gmail.com>
2021-11-18 18:40:46 -0300

    feat: add page to decide listing type

--- web/src/pages/tracks/[id]/list/index.tsx
+++ web/src/pages/tracks/[id]/list/index.tsx
@@ -1,8 +1,14 @@
+import { Button } from 'components/Button';
 import { BackButton } from 'components/Buttons/BackButton';
 import { Layout } from 'components/Layout';
 import { TopNavBarProps } from 'components/TopNavBar';
+import { Auction } from 'icons/Auction';
+import { CheckmarkFilled } from 'icons/CheckmarkFilled';
+import { useRouter } from 'next/router';
 
 export default function ListPage() {
+  const router = useRouter();
+
   const topNovaBarProps: TopNavBarProps = {
     leftButton: <BackButton />,
     title: 'List for Sale',
@@ -10,7 +16,50 @@ export default function ListPage() {
 
   return (
     <Layout topNavBarProps={topNovaBarProps}>
-      <div></div>
+      <div className="flex flex-col  gap-3">
+        <p className="text-gray-80 text-center font-bold text-sm mt-20">Select Listing Type:</p>
+        <div>
+          <div className="flex flex-col bg-black border-2 border-gray-700 rounded m-3 p-4 gap-3">
+            <div className="flex flex-row gap-2">
+              <CheckmarkFilled className="h-6 w-6" />
+              <p className="text-white font-bold text-sm">Buy it Now</p>
+            </div>
+            <p className="text-white text-xs">
+              With a fixed-price Buy It Now listing, a buyer knows the exact price they need to pay for your NFT and can
+              complete their purchase immediately. There is no bidding on fixed-price listings.
+            </p>
+            <Button
+              variant="outline"
+              borderColor="bg-green-gradient"
+              className="h-10 w-1/2"
+              onClick={() => router.push(`${router.asPath}/buy-now`)}
+            >
+              BUY NOW LISTING
+            </Button>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col bg-black border-2 border-gray-700 rounded m-3 p-4 gap-3">
+            <div className="flex flex-row gap-2">
+              <Auction className="h-6 w-6" />
+              <p className="text-white font-bold text-sm">Auction</p>
+            </div>
+            <p className="text-white text-xs">
+              When you list an NFT for sale in an auction, you choose a starting price, time limit, and a reserve price.
+              Interested buyers will place bids and when the auction ends, your NFT is sold to the highest bidder as
+              long as it meets the reserve price.
+            </p>
+            <Button
+              variant="outline"
+              borderColor="bg-purple-gradient"
+              className="h-10 w-1/2"
+              onClick={() => router.push(`${router.asPath}/auction`)}
+            >
+              AUCTION LISTING
+            </Button>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
