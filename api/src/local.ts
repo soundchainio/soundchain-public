fb7e9b254bf691478257e9347e9169f4ef2234ac
Vinicius Macelai <vinicius.macelai@gmail.com>
2021-11-18 18:10:12 -0300

    fix: use the right event on lambda

--- api/src/local.ts
+++ api/src/local.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   server.applyMiddleware({ app });
 
   await new Promise<void>(resolve => app.listen({ port: config.express.port }, resolve));
-  setInterval(() => watcher({}, undefined, null), 60 * 1000);
+  setInterval(() => watcher({}, undefined, null), 10 * 1000);
   // setInterval(() => playbackCount({}, undefined, null), 60 * 1000);
 
   console.log(`🚀 Server ready at http://localhost:${config.express.port}${server.graphqlPath}`);
