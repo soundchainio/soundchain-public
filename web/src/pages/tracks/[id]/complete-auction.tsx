e5210e28a65df637b53945b8e1cfaf1cca1ed211
Vinicius Macelai <vinicius.macelai@gmail.com>
2021-11-23 11:00:10 -0300

    feat: add complete auction page for buyer

--- web/src/pages/tracks/[id]/complete-auction.tsx
+++ web/src/pages/tracks/[id]/complete-auction.tsx
@@ -1,6 +1,6 @@
 import { Button } from 'components/Button';
 import { BackButton } from 'components/Buttons/BackButton';
-import { PlaceBid } from 'components/details-NFT/PlaceBid';
+import { AuctionEnded } from 'components/details-NFT/AuctionEnded';
 import { Layout } from 'components/Layout';
 import { TopNavBarProps } from 'components/TopNavBar';
 import { Track } from 'components/Track';
@@ -17,6 +17,11 @@ import { useEffect, useState } from 'react';
 
 export interface TrackPageProps {
   trackId: string;
+}
+
+export interface HighestBid {
+  bid: string;
+  bidder: string;
 }
 
 interface TrackPageParams extends ParsedUrlQuery {
@@ -40,17 +45,16 @@ export const getServerSideProps = protectPage<TrackPageProps, TrackPageParams>(a
     return { notFound: true };
   }
 
-  return cacheFor(PlaceBidPage, { trackId }, context, apolloClient);
+  return cacheFor(CompleteAuctionPage, { trackId }, context, apolloClient);
 });
 
-export default function PlaceBidPage({ trackId }: TrackPageProps) {
-  const { placeBid, getHighestBid } = useBlockchain();
+export default function CompleteAuctionPage({ trackId }: TrackPageProps) {
+  const { resultAuction, getHighestBid } = useBlockchain();
   const { data: track } = useTrackQuery({ variables: { id: trackId } });
   const { account, web3 } = useWalletContext();
   const maxGasFee = useMaxGasFee();
   const [loading, setLoading] = useState(false);
-  const [bidAmount, setBidAmount] = useState(0);
-  const [highestBid, setHighestBid] = useState('0');
+  const [highestBid, setHighestBid] = useState<HighestBid>({} as HighestBid);
   const me = useMe();
 
   const tokenId = track?.track.nftData?.tokenId ?? -1;
@@ -64,50 +68,41 @@ export default function PlaceBidPage({ trackId }: TrackPageProps) {
   }, [getAuctionItem]);
 
   useEffect(() => {
-    if (!web3) {
-      return;
-    }
     const fetchHighestBid = async () => {
-      const { _bid } = await getHighestBid(web3, tokenId);
-      setHighestBid(_bid);
+      if (!web3 || !tokenId || !getHighestBid || highestBid.bidder) {
+        return;
+      }
+      const { _bid, _bidder } = await getHighestBid(web3, tokenId);
+      setHighestBid({ bid: _bid, bidder: _bidder });
     };
     fetchHighestBid();
-    const interval = setInterval(() => {
-      fetchHighestBid();
-    }, 10 * 1000);
-
-    return () => clearInterval(interval);
-  }, [tokenId, trackId, web3, getHighestBid]);
+  }, [tokenId, web3, getHighestBid, highestBid.bidder]);
 
   if (!auctionItem) {
     return null;
   }
 
-  const ownerAddressAccount = 'listingPayload.buyNowItem?.buyNowItem?.owner.toLowerCase()';
-  const isOwner = ownerAddressAccount === account?.toLowerCase();
-  const isForSale = !!auctionItem.auctionItem.auctionItem?.reservePrice ?? false;
-  const price = '1';
+  const saleEnded = (auctionItem.auctionItem?.auctionItem?.endingTime || 0) < Math.floor(Date.now() / 1000);
 
-  const handlePlaceBid = () => {
-    if (
-      !web3 ||
-      !auctionItem.auctionItem?.auctionItem?.tokenId ||
-      !auctionItem.auctionItem?.auctionItem?.owner ||
-      !account
-    ) {
+  const handleClaim = () => {
+    if (!web3 || !auctionItem.auctionItem?.auctionItem?.tokenId || !account) {
       return;
     }
-    const amount = (bidAmount * 1e18).toString();
-    placeBid(web3, tokenId, account, amount, () => setLoading(false));
+    resultAuction(web3, tokenId, account, () => setLoading(false));
     setLoading(true);
   };
 
   const topNovaBarProps: TopNavBarProps = {
     leftButton: <BackButton />,
-    title: 'Place Bid',
+    title: 'Complete Auction',
   };
 
-  if (!isForSale || isOwner || !me || track?.track.nftData?.pendingRequest != PendingRequest.None) {
+  if (
+    account !== highestBid.bidder ||
+    !saleEnded ||
+    !me ||
+    track?.track.nftData?.pendingRequest != PendingRequest.None
+  ) {
     return null;
   }
 
@@ -116,15 +111,7 @@ export default function PlaceBidPage({ trackId }: TrackPageProps) {
       <div className="m-4">
         <Track trackId={trackId} />
       </div>
-      {price && ownerAddressAccount && (
-        <PlaceBid
-          highestBid={highestBid}
-          bidAmount={bidAmount}
-          onSetBidAmount={amount => setBidAmount(amount)}
-          ownerAddressAccount={ownerAddressAccount}
-          endingTime={auctionItem.auctionItem?.auctionItem?.endingTime ?? 0}
-        />
-      )}
+      <AuctionEnded highestBid={highestBid} />
       <div className="flex p-4">
         <div className="flex-1 font-black text-xs text-gray-80">
           <p>Max gas fee</p>
@@ -133,8 +120,8 @@ export default function PlaceBidPage({ trackId }: TrackPageProps) {
             <div className="text-white">{maxGasFee}</div>MATIC
           </div>
         </div>
-        <Button variant="buy-nft" onClick={handlePlaceBid} loading={loading}>
-          <div className="px-4">CONFIRM BID</div>
+        <Button variant="buy-nft" onClick={handleClaim} loading={loading}>
+          <div className="px-4">CLAIM NFT</div>
         </Button>
       </div>
     </Layout>
