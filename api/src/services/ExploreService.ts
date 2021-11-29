40d7a180fe3956d83a22858509994d8f2955915e
Jean Mayer <jean.mayer@ae.studio>
2021-11-29 10:42:28 -0300

    Sorting explore page results from newest to oldest

--- api/src/services/ExploreService.ts
+++ api/src/services/ExploreService.ts
@@ -4,6 +4,7 @@ import { Track } from '../models/Track';
 import { Context } from '../types/Context';
 import { ExplorePayload } from '../types/ExplorePayload';
 import { PageInput } from '../types/PageInput';
+import { SortOrder } from '../types/SortOrder';
 import { Service } from './Service';
 
 export class ExploreService extends Service {
@@ -14,16 +15,23 @@ export class ExploreService extends Service {
   async getExplore(search?: string): Promise<ExplorePayload> {
     const profiles = await this.context.profileService.searchProfiles(search);
     const tracks = await this.context.trackService.searchTracks(search);
-    return { profiles: profiles.list, totalProfiles: profiles.total, tracks: tracks.list, totalTracks: tracks.total, };
+    return { profiles: profiles.list, totalProfiles: profiles.total, tracks: tracks.list, totalTracks: tracks.total };
   }
 
   getExploreTracks(search: string, page?: PageInput): Promise<PaginateResult<Track>> {
     const regex = new RegExp(search, 'i');
-    return this.context.trackService.paginate({ filter: { $or: [{ title: regex }, { description: regex }, { artist: regex }, { album: regex }], deleted: false }, page });
+    return this.context.trackService.paginate({
+      filter: { $or: [{ title: regex }, { description: regex }, { artist: regex }, { album: regex }], deleted: false },
+      sort: { field: 'createdAt', order: SortOrder.DESC },
+      page,
+    });
   }
 
   getExploreUsers(search: string, page?: PageInput): Promise<PaginateResult<Profile>> {
-    return this.context.profileService.paginate({ filter: { displayName: new RegExp(search, 'i') }, page });
+    return this.context.profileService.paginate({
+      filter: { displayName: new RegExp(search, 'i') },
+      sort: { field: 'createdAt', order: SortOrder.DESC },
+      page,
+    });
   }
-
 }
