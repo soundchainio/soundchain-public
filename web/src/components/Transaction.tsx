91b7b6ef726a8768164c7582395b2be60afd14d6
Fernando Goncalves <fernando.hmg@gmail.com>
2021-12-02 11:46:06 -0300

    Moving the transaction types to another file

--- web/src/components/Transaction.tsx
+++ web/src/components/Transaction.tsx
@@ -1,17 +1,6 @@
-import { AuctionCanceled } from 'icons/AuctionCanceled';
-import { AuctionCreated } from 'icons/AuctionCreated';
-import { AuctionPlaceBid } from 'icons/AuctionPlaceBid';
-import { AuctionResult } from 'icons/AuctionResult';
-import { Checkmark2 } from 'icons/Checkmark2';
-import { Fire } from 'icons/Fire';
-import { ListCanceled } from 'icons/ListCanceled';
-import { ListUpdated } from 'icons/ListUpdated';
-import { Marketplace } from 'icons/Marketplace';
 import { Matic } from 'icons/Matic';
-import { PriceTag } from 'icons/PriceTag';
-import { Stars } from 'icons/Stars';
-import { Transfer } from 'icons/Transfer';
 import { PolygonscanResultObj } from 'lib/graphql';
+import { transactionDataMap } from 'types/TransactionType';
 import { currency } from 'utils/format';
 
 interface TransactionProps {
@@ -19,29 +8,8 @@ interface TransactionProps {
   maticUsdValue?: string;
 }
 
-interface TransactionData {
-  methodName: string;
-  icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
-}
-
-const map: Record<string, TransactionData> = {
-  listItem: { methodName: 'Listed NFT', icon: PriceTag },
-  safeMint: { methodName: 'Minted NFT', icon: Stars },
-  buyItem: { methodName: 'Bought NFT', icon: Checkmark2 },
-  setApprovalForAll: { methodName: 'Approve Marketplace', icon: Marketplace },
-  transfer: { methodName: 'Transfer', icon: Transfer },
-  createAuction: { methodName: 'Auction created', icon: AuctionCreated },
-  burn: { methodName: 'Burn/Delete NFT', icon: Fire },
-  cancelListing: { methodName: 'Listed NFT canceled', icon: ListCanceled },
-  updateListing: { methodName: 'Listed NFT updated', icon: ListUpdated },
-  transferFrom: { methodName: 'Transfered NFT', icon: Transfer },
-  cancelAuction: { methodName: 'Auction canceled', icon: AuctionCanceled },
-  placeBid: { methodName: 'Place bid', icon: AuctionPlaceBid },
-  resultAuction: { methodName: 'Result auction', icon: AuctionResult },
-};
-
 export const Transaction = ({ transaction, maticUsdValue }: TransactionProps) => {
-  const Icon = transaction.method && map[transaction.method].icon;
+  const Icon = transaction.method && transactionDataMap[transaction.method].icon;
 
   const transactionValue = parseInt(transaction.value) / 1e18;
   const gasFee = (parseInt(transaction.gasUsed) * parseInt(transaction.gasPrice)) / 1e18;
@@ -61,7 +29,7 @@ export const Transaction = ({ transaction, maticUsdValue }: TransactionProps) =>
       {Icon && <Icon />}
       <div>
         <p className="text-white text-xs font-bold">
-          {(transaction.method && map[transaction.method]?.methodName) || 'Unknown'}
+          {(transaction.method && transactionDataMap[transaction.method]?.methodName) || 'Unknown'}
         </p>
         <p className="text-gray-80 text-xxs font-medium">{transaction.date}</p>
       </div>
