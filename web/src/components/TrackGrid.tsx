69ea28092cb0ae1477bede7608072eb01321a864
Vinicius Macelai <vinicius.macelai@gmail.com>
2021-12-08 17:50:10 -0300

    fix: make whole card cliclable

--- web/src/components/TrackGrid.tsx
+++ web/src/components/TrackGrid.tsx
@@ -36,7 +36,7 @@ export const TrackGrid = ({ track }: TrackProps) => {
   };
   const { listingItem } = track;
   const saleType = getSaleType(listingItem);
-  const price = listingItem?.priceToShow ?? '';
+  const price = listingItem?.priceToShow ?? 0;
   const { art, artist, title, trackId, playbackCount, favoriteCount } = song;
   const { play, isCurrentSong, isCurrentlyPlaying } = useAudioPlayerContext();
   const [isPlaying, setIsPlaying] = useState(false);
@@ -46,14 +46,14 @@ export const TrackGrid = ({ track }: TrackProps) => {
   }, [isCurrentSong, isCurrentlyPlaying, setIsPlaying, trackId]);
 
   return (
-    <div className="p-0.5 rounded-xl bg-rainbow-gradient">
-      <div className="bg-black rounded-lg p-4 items-center">
-        <div className="flex justify-center items-center mb-2">
-          <div className="h-24 w-24 relative">
-            <Asset src={art} />
+    <NextLink href={`/tracks/${trackId}`}>
+      <div className="p-0.5 rounded-xl bg-rainbow-gradient cursor-pointer">
+        <div className="bg-black rounded-lg p-4 items-center ">
+          <div className="flex justify-center items-center mb-2">
+            <div className="h-24 w-24 relative">
+              <Asset src={art} />
+            </div>
           </div>
-        </div>
-        <NextLink href={`/tracks/${trackId}`}>
           <div className="flex w-full cursor-pointer truncate justify-center mb-2">
             <div className="truncate">
               <div className="text-white font-black text-xs truncate text-center">
@@ -68,34 +68,34 @@ export const TrackGrid = ({ track }: TrackProps) => {
               </div>
             </div>
           </div>
-        </NextLink>
-        <div className="flex items-center gap-3">
-          <div className="flex flex-col flex-1 truncate">
-            <div className="flex items-center gap-1">
-              {saleType && saleType !== '' && (
-                <>
-                  <div className="text-white font-bold text-xxs">{parseInt(price) / 1e18}</div>
-                  <div className="flex items-center justify-center flex-shrink-0 h-full">
-                    <Matic height="12" width="12" />
-                  </div>
-                  <BadgeTrack auction={saleType === 'auction'} label={saleType.toUpperCase()}></BadgeTrack>
-                </>
-              )}
+          <div className="flex items-center gap-3">
+            <div className="flex flex-col flex-1 truncate">
+              <div className="flex items-center gap-1">
+                {saleType && saleType !== '' && (
+                  <>
+                    <div className="text-white font-bold text-xxs">{price / 1e18}</div>
+                    <div className="flex items-center justify-center flex-shrink-0 h-full">
+                      <Matic height="12" width="12" />
+                    </div>
+                    <BadgeTrack auction={saleType === 'auction'} label={saleType.toUpperCase()}></BadgeTrack>
+                  </>
+                )}
+              </div>
+              <div className="text-gray-80 text-xs flex gap-1 items-center pt-1">
+                <Play fill="#808080" />
+                <span>{playbackCount || 0}</span>
+                <HeartFilled />
+                <span>{favoriteCount || 0}</span>
+              </div>
             </div>
-            <div className="text-gray-80 text-xs flex gap-1 items-center pt-1">
-              <Play fill="#808080" />
-              <span>{playbackCount || 0}</span>
-              <HeartFilled />
-              <span>{favoriteCount || 0}</span>
+            <div className="flex items-center">
+              <button className="bg-white rounded-full w-6 h-6 flex items-center" onClick={() => play(song)}>
+                {isPlaying ? <Pause className="text-white m-auto scale-125" /> : <Play className="text-white m-auto" />}
+              </button>
             </div>
-          </div>
-          <div className="flex items-center">
-            <button className="bg-white rounded-full w-6 h-6 flex items-center" onClick={() => play(song)}>
-              {isPlaying ? <Pause className="text-white m-auto scale-125" /> : <Play className="text-white m-auto" />}
-            </button>
           </div>
         </div>
       </div>
-    </div>
+    </NextLink>
   );
 };
