6ddac6b05b5ce1afedbe2669766ab21d45c4ed1c
Vinicius Macelai <vinicius.macelai@gmail.com>
2021-12-10 19:30:32 -0300

    fix: playback and createdAt should be desceding

--- web/src/components/Marketplace.tsx
+++ web/src/components/Marketplace.tsx
@@ -17,8 +17,8 @@ enum SortListingItem {
 const SelectToApolloQuery: Record<SortListingItem, { field: SortListingItemField; order: SortOrder }> = {
   [SortListingItem.PriceAsc]: { field: SortListingItemField.Price, order: SortOrder.Asc },
   [SortListingItem.PriceDesc]: { field: SortListingItemField.Price, order: SortOrder.Desc },
-  [SortListingItem.PlaybackCount]: { field: SortListingItemField.PlaybackCount, order: SortOrder.Asc },
-  [SortListingItem.CreatedAt]: { field: SortListingItemField.CreatedAt, order: SortOrder.Asc },
+  [SortListingItem.PlaybackCount]: { field: SortListingItemField.PlaybackCount, order: SortOrder.Desc },
+  [SortListingItem.CreatedAt]: { field: SortListingItemField.CreatedAt, order: SortOrder.Desc },
 };
 
 export const Marketplace = () => {
