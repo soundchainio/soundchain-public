795c9b735f274bde7b600fc6e8a33facc0b5d3e6
Vinicius Borges <vinicius.borges@ae.studio>
2021-12-28 16:40:24 -0300

    refactor(2fa): two-factor screen

--- web/src/components/forms/profile/two-factor/ValidateCodeForm.tsx
+++ web/src/components/forms/profile/two-factor/ValidateCodeForm.tsx
@@ -5,7 +5,7 @@ export const ValidateCodeForm = () => {
   return (
     <div className="flex flex-col flex-grow">
       <Label className="text-white mb-4">To validate the secret, enter the 6-digit code from the app</Label>
-      <div className="mt-2 flex justify-center gap-4">
+      <div className="mt-2 flex flex-col justify-center gap-4">
         <InputField type="text" name="token" maxLength={6} />
       </div>
     </div>
