c3f751959231ce3545753e54829619ddda56158f
Vinicius Borges <vinicius.borges@ae.studio>
2021-12-29 09:13:36 -0300

    refactor(2fa): make input as number

--- web/src/components/forms/profile/two-factor/ValidateCodeForm.tsx
+++ web/src/components/forms/profile/two-factor/ValidateCodeForm.tsx
@@ -1,12 +1,11 @@
-import { Label } from 'components/Label';
 import { InputField } from 'components/InputField';
 
 export const ValidateCodeForm = () => {
   return (
     <div className="flex flex-col flex-grow">
-      <Label className="text-white mb-4">To validate the secret, enter the 6-digit code from the app</Label>
+      <p className="text-white mb-4">To validate the secret, enter the 6-digit code from the app</p>
       <div className="mt-2 flex flex-col justify-center gap-4">
-        <InputField type="text" name="token" maxLength={6} />
+        <InputField type="number" name="token" max={999999} />
       </div>
     </div>
   );
