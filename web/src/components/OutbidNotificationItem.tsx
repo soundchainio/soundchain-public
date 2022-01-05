910cd7c5bd1213e1db87c499290e6d5a188e1903
Vinicius Macelai <vinicius.macelai@gmail.com>
2022-01-05 11:34:47 -0300

    refactor: create new component for track notification

--- web/src/components/OutbidNotificationItem.tsx
+++ web/src/components/OutbidNotificationItem.tsx
@@ -1,10 +1,5 @@
-import classNames from 'classnames';
-import { Matic } from 'icons/Matic';
 import { OutbidNotification } from 'lib/graphql';
-import NextLink from 'next/link';
-import React from 'react';
-import Asset from './Asset';
-import { Timestamp } from './Timestamp';
+import { TrackNotification } from './TrackNotification';
 
 interface NotificationProps {
   notification: OutbidNotification;
@@ -15,28 +10,17 @@ export const OutbidNotificationItem = ({
   notification: { createdAt, price, trackId, trackName, artist, artworkUrl },
   index,
 }: NotificationProps) => {
+  const title = 'You have been outbid! You have to bid higher!';
   return (
-    <div className={classNames('cursor-pointer flex flex-col p-4', index % 2 === 0 ? 'bg-gray-25' : 'bg-gray-20')}>
-      <div className="break-words flex flex-col">
-        <div className="text-gray-100 text-sm items-center w-full inline-block">
-          You have been outbid! You have to bid higher!
-        </div>
-        <Timestamp small datetime={createdAt} className="text-sm" />
-      </div>
-      <NextLink href={`/tracks/${trackId}`}>
-        <div className="flex items-center gap-3 p-3 bg-gray-30 rounded-lg cursor-pointer mt-4">
-          <div className="w-10 h-10 relative">
-            <Asset src={artworkUrl} sizes="2.5rem" />
-          </div>
-          <div>
-            <div className="text-white font-bold text-xs">{trackName}</div>
-            <div className="text-gray-80 font-bold text-xs">{artist}</div>
-          </div>
-          <div className="flex gap-2 text-white font-bold text-xs ml-auto">
-            <Matic /> {price / 1e18} MATIC
-          </div>
-        </div>
-      </NextLink>
-    </div>
+    <TrackNotification
+      createdAt={createdAt}
+      price={price}
+      trackId={trackId}
+      trackName={trackName}
+      artist={artist}
+      artworkUrl={artworkUrl}
+      index={index}
+      title={title}
+    />
   );
 };
