6d38e51807a615be83083e0f232765eb5a598e7e
Vinicius Macelai <vinicius.macelai@gmail.com>
2022-01-11 15:35:15 -0300

    refactor: remove useless mint function on lambda

--- api/src/lambda.ts
+++ api/src/lambda.ts
@@ -1,9 +1,6 @@
-import { createAlchemyWeb3 } from '@alch/alchemy-web3';
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import pinataClient from '@pinata/sdk';
 import { mongoose } from '@typegoose/typegoose';
 import { ApolloServer } from 'apollo-server-lambda';
-import type { Handler, SQSEvent } from 'aws-lambda';
+import type { Handler } from 'aws-lambda';
 import express from 'express';
 import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
@@ -24,7 +21,6 @@ import muxDataApi from './muxDataApi';
 import { ItemCanceled, ItemListed, ItemSold, Transfer } from './types/BlockchainEvents';
 import { Context } from './types/Context';
 import { MuxDataInputValue, MuxServerData } from './types/MuxData';
-import { Metadata, NFT } from './types/NFT';
 import { PendingRequest } from './types/PendingRequest';
 
 export const handler: Handler = async (...args) => {
@@ -310,64 +306,6 @@ export const watcher: Handler = async () => {
     }
   }
   await context.blockTrackerService.updateCurrentBlocknumber(toBlock + 1);
-};
-
-export const mint: Handler<SQSEvent> = async event => {
-  if (event.Records.length !== 1) {
-    console.error('Event message should contain exactly ONE record');
-    return;
-  }
-
-  const pinToIPFS = async (key: string, name: string) => {
-    const s3Client = new S3Client({ region: config.uploads.region, forcePathStyle: true });
-    const command = new GetObjectCommand({ Bucket: config.uploads.bucket, Key: key });
-    const response = await s3Client.send(command);
-    const assetResult = await pinata.pinFileToIPFS(response.Body, {
-      pinataMetadata: {
-        name: name,
-      },
-    });
-    return assetResult;
-  };
-
-  const web3 = createAlchemyWeb3(config.minting.alchemyKey);
-  const pinata = pinataClient(config.minting.pinataKey, config.minting.pinataSecret);
-
-  try {
-    const body: NFT = JSON.parse(event.Records[0].body);
-    const { assetKey, artKey, to, ...nft } = body;
-    const name = body.name;
-    const contract = new web3.eth.Contract(SoundchainCollectible.abi as AbiItem[], config.minting.nftAddress);
-
-    const assetResult = await pinToIPFS(assetKey, name);
-
-    const metadata: Metadata = { ...nft, asset: `ipfs://${assetResult.IpfsHash}` };
-
-    if (artKey) {
-      const artResult = await pinToIPFS(artKey, `${name}-preview`);
-      metadata.art = `ipfs://${artResult.IpfsHash}`;
-    }
-
-    const metadataResult = await pinata.pinJSONToIPFS(metadata, { pinataMetadata: { name: `${name}-metadata` } });
-
-    const nonce = await web3.eth.getTransactionCount(config.minting.walletPublicKey, 'latest'); //get latest nonce
-
-    const transaction = {
-      from: config.minting.walletPublicKey,
-      to: config.minting.nftAddress,
-      nonce,
-      gas: 260000, // minimum
-      data: contract.methods.safeMint(to, `ipfs://${metadataResult.IpfsHash}`).encodeABI(),
-    };
-
-    const signedTransaction = await web3.eth.accounts.signTransaction(transaction, config.minting.walletPrivateKey);
-
-    const receipt = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction as string);
-    console.log(receipt.transactionHash);
-  } catch (e) {
-    console.error('Execution error, please check AWS logs', e);
-    process.exit(1);
-  }
 };
 
 export const playbackCount: Handler = async () => {
