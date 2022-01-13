06c9d4b4456f68926bb9d0efc37173c3fee81407
Vinicius Macelai <vinicius.macelai@gmail.com>
2022-01-13 16:49:38 -0300

    feat: add decorator to log failed event

--- api/src/lambda/processEvents.ts
+++ api/src/lambda/processEvents.ts
@@ -1,10 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Transfer } from '../../types/web3-v1-contracts/Soundchain721';
+import { FailedEventModel } from '../models/FailedEvent';
 import { Context } from '../types/Context';
 import { PendingRequest } from '../types/PendingRequest';
 
 const zeroAddress = '0x0000000000000000000000000000000000000000';
 
-export const itemListed = async (returnValues: any, context: Context): Promise<void> => {
+function _execute(f: (event: any, context: Context) => Promise<void>) {
+  return async function (this: any, ...args: [event: any, context: Context]) {
+    const a: [event: any, context: Context] = [...args];
+    try {
+      await f.apply(this, a);
+    } catch (e: any) {
+      const failedEvent = new FailedEventModel({ name: f.name, error: e.message, data: a[0] });
+      console.log({ failedEvent });
+      await failedEvent.save();
+    }
+  };
+}
+
+export const processItemListed = async (returnValues: any, context: Context): Promise<void> => {
   try {
     const { owner, nft, tokenId, pricePerItem, startingTime } = returnValues;
     const [user, listedBefore] = await Promise.all([
@@ -34,8 +49,9 @@ export const itemListed = async (returnValues: any, context: Context): Promise<v
   }
   console.log('ItemListed');
 };
+export const itemListed = _execute(processItemListed);
 
-export const itemSold = async (returnValues: any, context: Context): Promise<void> => {
+export const processItemSold = async (returnValues: any, context: Context): Promise<void> => {
   try {
     const { tokenId, seller, buyer, pricePerItem } = returnValues;
     await context.buyNowItemService.finishListing(tokenId, seller, buyer, parseInt(pricePerItem));
@@ -44,8 +60,9 @@ export const itemSold = async (returnValues: any, context: Context): Promise<voi
   }
   console.log('ItemSold');
 };
+export const itemSold = _execute(processItemSold);
 
-export const itemUpdated = async (returnValues: any, context: Context): Promise<void> => {
+export const processItemUpdated = async (returnValues: any, context: Context): Promise<void> => {
   try {
     const { tokenId, newPrice, startingTime } = returnValues;
     await Promise.all([
@@ -60,8 +77,9 @@ export const itemUpdated = async (returnValues: any, context: Context): Promise<
   }
   console.log('ItemUpdated');
 };
+export const itemUpdated = _execute(processItemUpdated);
 
-export const itemCanceled = async (returnValues: any, context: Context): Promise<void> => {
+export const processItemCanceled = async (returnValues: any, context: Context): Promise<void> => {
   try {
     const { tokenId } = returnValues;
     await context.buyNowItemService.setNotValid(parseInt(tokenId));
@@ -71,31 +89,29 @@ export const itemCanceled = async (returnValues: any, context: Context): Promise
   }
   console.log('ItemCanceled');
 };
+export const itemCanceled = _execute(processItemCanceled);
 
-export const transfer = async (event: any, context: Context): Promise<void> => {
-  try {
-    const { transactionHash, address, returnValues } = event as unknown as Transfer;
-    if (returnValues.from === zeroAddress) {
-      await context.trackService.updateTrackByTransactionHash(transactionHash, {
-        nftData: {
-          tokenId: parseInt(returnValues.tokenId),
-          contract: address,
-          pendingRequest: PendingRequest.None,
-        },
-      });
-    } else if (returnValues.to === zeroAddress) {
-      const track = await context.trackService.getTrackByTokenId(parseInt(returnValues.tokenId));
-      await context.trackService.deleteTrackByAdmin(track._id);
-    } else {
-      await context.trackService.updateOwnerByTokenId(parseInt(returnValues.tokenId), returnValues.to);
-    }
-  } catch (error) {
-    console.error(error);
+const processTransfer = async (event: any, context: Context): Promise<void> => {
+  const { transactionHash, address, returnValues } = event as unknown as Transfer;
+  if (returnValues.from === zeroAddress) {
+    await context.trackService.updateTrackByTransactionHash(transactionHash, {
+      nftData: {
+        tokenId: parseInt(returnValues.tokenId),
+        contract: address,
+        pendingRequest: PendingRequest.None,
+      },
+    });
+  } else if (returnValues.to === zeroAddress) {
+    const track = await context.trackService.getTrackByTokenId(parseInt(returnValues.tokenId));
+    await context.trackService.deleteTrackByAdmin(track._id);
+  } else {
+    await context.trackService.updateOwnerByTokenId(parseInt(returnValues.tokenId), returnValues.to);
   }
   console.log('Transfer');
 };
+export const transfer = _execute(processTransfer);
 
-export const auctionCreated = async (returnValues: any, context: Context): Promise<void> => {
+export const processAuctionCreated = async (returnValues: any, context: Context): Promise<void> => {
   try {
     const { nftAddress, tokenId, owner, reservePrice, startTimestamp, endTimestamp } = returnValues;
     const [user, listedBefore] = await Promise.all([
@@ -126,8 +142,9 @@ export const auctionCreated = async (returnValues: any, context: Context): Promi
   }
   console.log('AuctionCreated');
 };
+export const auctionCreated = _execute(processAuctionCreated);
 
-export const bidPlaced = async (returnValues: any, context: Context): Promise<void> => {
+export const processBidPlaced = async (returnValues: any, context: Context): Promise<void> => {
   try {
     const { nftAddress, tokenId, bidder, bid } = returnValues;
     const tokenIdAsNumber = parseInt(tokenId);
@@ -177,8 +194,9 @@ export const bidPlaced = async (returnValues: any, context: Context): Promise<vo
   }
   console.log('BidPlaced');
 };
+export const bidPlaced = _execute(processBidPlaced);
 
-export const auctionResulted = async (returnValues: any, context: Context): Promise<void> => {
+export const processAuctionResulted = async (returnValues: any, context: Context): Promise<void> => {
   try {
     const { tokenId, winner, oldOwner, winningBid } = returnValues;
     await context.auctionItemService.finishListing(tokenId, oldOwner, winner, parseInt(winningBid));
@@ -187,8 +205,9 @@ export const auctionResulted = async (returnValues: any, context: Context): Prom
   }
   console.log('AuctionResulted');
 };
+export const auctionResulted = _execute(processAuctionResulted);
 
-export const auctionCanceled = async (returnValues: any, context: Context): Promise<void> => {
+export const processAuctionCanceled = async (returnValues: any, context: Context): Promise<void> => {
   try {
     const { tokenId } = returnValues;
     await Promise.all([
@@ -200,8 +219,9 @@ export const auctionCanceled = async (returnValues: any, context: Context): Prom
   }
   console.log('AuctionCancelled');
 };
+export const auctionCanceled = _execute(processAuctionCanceled);
 
-export const updateAuction = async (returnValues: any, context: Context): Promise<void> => {
+export const processUpdateAuction = async (returnValues: any, context: Context): Promise<void> => {
   try {
     const { tokenId, reservePrice, startTime, endTime } = returnValues;
     await Promise.all([
@@ -217,3 +237,4 @@ export const updateAuction = async (returnValues: any, context: Context): Promis
   }
   console.log('UpdateAuction');
 };
+export const updateAuction = _execute(processUpdateAuction);
