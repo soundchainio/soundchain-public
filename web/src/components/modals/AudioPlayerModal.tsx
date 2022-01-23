bf0bdc26f9d0f6076a471d5c056c31f217158b30
Mateus Batista <mateusalfredobatista@gmail.com>
2022-01-23 01:29:19 -0300

    nice tweaks

--- web/src/components/modals/AudioPlayerModal.tsx
+++ web/src/components/modals/AudioPlayerModal.tsx
@@ -84,110 +84,117 @@ export const AudioPlayerModal = () => {
       }
       onClose={handleClose}
     >
-      <div className="flex flex-col h-full justify-center items-center text-white">
-        <div className="w-full sm:max-w-xs px-8 sm:px-0">
-          <div className={isPlaylistOpen ? 'flex items-center gap-4' : 'block'}>
-            <div className="flex justify-center">
-              <div
-                className={
-                  isPlaylistOpen
-                    ? 'relative w-10 h-10 rounded-lg overflow-hidden'
-                    : 'relative w-3/4 max-h-80 sm:w-full after:block after:pb-full flex bg-gray-80 rounded-lg overflow-hidden'
-                }
-              >
-                <Asset src={currentSong.art} />
+      <div className="flex flex-col h-full items-center text-white">
+        <div className="w-full h-full sm:max-w-xs px-8 sm:px-0">
+          <div style={{ display: 'grid', height: '80vh', gridTemplateRows: '75% 25%' }}>
+            <div className={`flex flex-col ${isPlaylistOpen ? 'justify-start' : 'justify-end'}`}>
+              <div className={isPlaylistOpen ? 'flex items-center gap-4' : 'block'}>
+                <div className="flex justify-center">
+                  <div
+                    className={
+                      isPlaylistOpen
+                        ? 'relative w-10 h-10 rounded-lg overflow-hidden'
+                        : 'relative w-full max-h-80 sm:w-full after:block after:pb-full flex bg-gray-80 rounded-lg overflow-hidden'
+                    }
+                  >
+                    <Asset src={currentSong.art} />
+                  </div>
+                </div>
+                <div className="flex justify-between my-4 w-full">
+                  <div className="flex w-full gap-4">
+                    <NextLink href={`/tracks/${currentSong.trackId}`}>
+                      <a className="flex flex-col flex-1 min-w-0">
+                        <div className="flex items-center gap-2 min-w-0">
+                          <h2 className="font-black truncate">{currentSong.title || 'Unknown title'}</h2>
+                          <Info className="flex-shrink-0" />
+                        </div>
+                        <h3 className="font-medium">{currentSong.artist || 'Unknown artist'}</h3>
+                      </a>
+                    </NextLink>
+                    <button className="flex items-center" onClick={handleFavorite}>
+                      {isFavorite && <HeartFull />}
+                      {!isFavorite && <HeartBorder />}
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className={isPlaylistOpen ? 'visible mb-5 overflow-y-auto' : 'hidden'}>
+                <div>Playlist</div>
+                <div className="overflow-y-auto">
+                  {playlist.map((song, idx) => (
+                    <div key={song.trackId} className="sm:pr-2">
+                      <TrackListItem
+                        variant="playlist"
+                        index={idx + 1}
+                        song={song}
+                        handleOnPlayClicked={() => jumpTo(idx)}
+                      />
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-            <div className="flex justify-between my-4 w-full">
-              <div className="flex w-full gap-4">
-                <NextLink href={`/tracks/${currentSong.trackId}`}>
-                  <a className="flex flex-col flex-1 min-w-0">
-                    <div className="flex items-center gap-2 min-w-0">
-                      <h2 className="font-black truncate">{currentSong.title || 'Unknown title'}</h2>
-                      <Info className="flex-shrink-0" />
-                    </div>
-                    <h3 className="font-medium">{currentSong.artist || 'Unknown artist'}</h3>
-                  </a>
-                </NextLink>
-                <button className="flex items-center" onClick={handleFavorite}>
-                  {isFavorite && <HeartFull />}
-                  {!isFavorite && <HeartBorder />}
+            <div>
+              <Slider className="audio-player" min={0} max={duration} value={progress} onChange={onSliderChange} />
+              <div className="flex justify-between mt-2 text-xs text-gray-80 cursor-default">
+                <div>{timeFromSecs(progress || 0)}</div>
+                <div onClick={onPlaybackDurationClick}>
+                  {showTotalPlaybackDuration ? timeFromSecs(duration || 0) : remainingTime(progress, duration || 0)}
+                </div>
+              </div>
+              <div className="flex justify-between items-center mt-8">
+                <TrackShareButton
+                  trackId={currentSong.trackId}
+                  title={currentSong.title}
+                  artist={currentSong.artist}
+                  position="top-right"
+                />
+                <div className="flex justify-center gap-4">
+                  <button
+                    className={'rounded-full w-12 h-12 flex justify-center items-center'}
+                    aria-label="Previous track"
+                    onClick={playPrevious}
+                  >
+                    <Rewind className={'hover:fill-current active:text-gray-80'} />
+                  </button>
+                  <button
+                    className="bg-white rounded-full w-12 h-12 flex justify-center items-center hover:scale-110 active:scale-100"
+                    aria-label={isPlaying ? 'Pause' : 'Play'}
+                    onClick={togglePlay}
+                  >
+                    {isPlaying ? <Pause fill="black" /> : <Play fill="black" />}
+                  </button>
+                  <button
+                    className={`${
+                      !hasNext && 'cursor-default'
+                    } rounded-full w-12 h-12 flex justify-center items-center`}
+                    aria-label="Next track"
+                    onClick={playNext}
+                    disabled={!hasNext}
+                  >
+                    <Forward className={`${hasNext && 'hover:fill-current'} active:text-gray-80`} />
+                  </button>
+                </div>
+                <button className="w-5 text-gray-80" onClick={() => setIsPlaylistOpen(isOpen => !isOpen)}>
+                  <ViewListIcon />
                 </button>
               </div>
-            </div>
-          </div>
-          <div className={isPlaylistOpen ? 'visible mb-5' : 'hidden'}>
-            <div>Playlist</div>
-            <div className="max-h-56 overflow-y-auto">
-              {playlist.map((song, idx) => (
-                <div key={song.trackId} className="md:pr-2">
-                  <TrackListItem
-                    variant="playlist"
-                    index={idx + 1}
-                    song={song}
-                    handleOnPlayClicked={() => jumpTo(idx)}
+              <div className="hidden md:flex items-center gap-4 pt-8">
+                <VolumeOffIcon width={16} viewBox="-8 0 20 20" />
+                <div className="flex-1">
+                  <Slider
+                    className="volume-slider"
+                    min={0}
+                    max={1}
+                    value={volume}
+                    onChange={value => setVolume(value)}
+                    step={0.1}
                   />
                 </div>
-              ))}
+                <VolumeUpIcon width={16} />
+              </div>
             </div>
           </div>
-          <Slider className="audio-player" min={0} max={duration} value={progress} onChange={onSliderChange} />
-          <div className="flex justify-between mt-2 text-xs text-gray-80 cursor-default">
-            <div>{timeFromSecs(progress || 0)}</div>
-            <div onClick={onPlaybackDurationClick}>
-              {showTotalPlaybackDuration ? timeFromSecs(duration || 0) : remainingTime(progress, duration || 0)}
-            </div>
-          </div>
-          <div className="flex justify-between items-center mt-8">
-            <TrackShareButton
-              trackId={currentSong.trackId}
-              title={currentSong.title}
-              artist={currentSong.artist}
-              position="top-right"
-            />
-            <div className="flex justify-center gap-4">
-              <button
-                className={'rounded-full w-12 h-12 flex justify-center items-center'}
-                aria-label="Previous track"
-                onClick={playPrevious}
-              >
-                <Rewind className={'hover:fill-current active:text-gray-80'} />
-              </button>
-              <button
-                className="bg-white rounded-full w-12 h-12 flex justify-center items-center hover:scale-110 active:scale-100"
-                aria-label={isPlaying ? 'Pause' : 'Play'}
-                onClick={togglePlay}
-              >
-                {isPlaying ? <Pause fill="black" /> : <Play fill="black" />}
-              </button>
-              <button
-                className={`${!hasNext && 'cursor-default'} rounded-full w-12 h-12 flex justify-center items-center`}
-                aria-label="Next track"
-                onClick={playNext}
-                disabled={!hasNext}
-              >
-                <Forward className={`${hasNext && 'hover:fill-current'} active:text-gray-80`} />
-              </button>
-            </div>
-            <button className="w-5 text-gray-80" onClick={() => setIsPlaylistOpen(isOpen => !isOpen)}>
-              <ViewListIcon />
-            </button>
-          </div>
-          <div className="hidden md:flex items-center gap-4 pt-8">
-            <VolumeOffIcon width={16} viewBox="-8 0 20 20" />
-            <div className="flex-1">
-              <Slider
-                className="volume-slider"
-                min={0}
-                max={1}
-                value={volume}
-                onChange={value => setVolume(value)}
-                step={0.1}
-              />
-            </div>
-            <VolumeUpIcon width={16} />
-          </div>
-          <div className="py-4 flex justify-center"></div>
         </div>
       </div>
     </Modal>
