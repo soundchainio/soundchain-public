cc50f1e08d8910d11c4886b4690be3934cfc08dd
Fernando Goncalves <fernando.hmg@gmail.com>
2022-01-31 17:11:56 -0300

    replacing placeholder gif for a webm video

--- web/src/components/Asset.tsx
+++ web/src/components/Asset.tsx
@@ -1,7 +1,7 @@
 import { LogoBase64 } from 'components/LogoBase64';
 import { useMimeTypeQuery } from 'lib/graphql';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface AssetProps {
   src?: string | null;
@@ -9,6 +9,7 @@ interface AssetProps {
 }
 
 const Asset = ({ src, sizes }: AssetProps) => {
+  const [isLoaded, setIsLoaded] = useState(false);
   const { data } = useMimeTypeQuery({
     variables: { url: src as string },
     skip: !src,
@@ -20,18 +21,33 @@ const Asset = ({ src, sizes }: AssetProps) => {
 
   if (src && mimeType?.startsWith('video')) {
     return (
-      <video
-        src={src}
-        loop
-        muted
-        autoPlay
-        playsInline
-        disablePictureInPicture
-        disableRemotePlayback
-        poster="/animations/bg-video.gif"
-        className="w-full object-cover"
-        style={{ height: 'inherit' }}
-      />
+      <>
+        <video
+          src={src}
+          loop
+          muted
+          autoPlay
+          playsInline
+          disablePictureInPicture
+          disableRemotePlayback
+          className={`w-full object-cover relative ${!isLoaded ? 'hidden' : ''}`}
+          style={{ height: 'inherit' }}
+          onLoadedData={() => setIsLoaded(true)}
+        />
+        {!isLoaded && (
+          <video
+            src="/animations/bg-video.webm"
+            loop
+            muted
+            autoPlay
+            playsInline
+            disablePictureInPicture
+            disableRemotePlayback
+            className="w-full object-cover absolute inset-0"
+            style={{ height: 'inherit' }}
+          />
+        )}
+      </>
     );
   }
 
