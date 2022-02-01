f7a51c74a346aba98102182d6d12cf9337296e01
Vinicius Borges <vinicius.borges@ae.studio>
2022-02-01 10:52:22 -0300

    refactor: remove unnecessary useEffect

--- web/src/components/Marketplace.tsx
+++ web/src/components/Marketplace.tsx
@@ -1,9 +1,18 @@
+/* eslint-disable react/display-name */
+import { ApolloQueryResult } from '@apollo/client';
 import { useModalDispatch, useModalState } from 'contexts/providers/modal';
 import { Filter } from 'icons/Filter';
-import { GridView } from 'icons/GridView';
-import { ListView } from 'icons/ListView';
-import { SortListingItemField, SortOrder, TrackQuery, TrackWithListingItem, useListingItemsQuery } from 'lib/graphql';
-import React, { useEffect, useState } from 'react';
+import { GridView as GridViewIcon } from 'icons/GridView';
+import { ListView as ListViewIcon } from 'icons/ListView';
+import {
+  ListingItemsQuery,
+  SortListingItemField,
+  SortOrder,
+  TrackQuery,
+  TrackWithListingItem,
+  useListingItemsQuery,
+} from 'lib/graphql';
+import React, { Dispatch, memo, SetStateAction, useEffect, useState } from 'react';
 import PullToRefresh from 'react-simple-pull-to-refresh';
 import { GenreLabel } from 'utils/Genres';
 import { SaleTypeLabel } from 'utils/SaleTypeLabel';
@@ -29,50 +38,28 @@ const SelectToApolloQuery: Record<SortListingItem, { field: SortListingItemField
   [SortListingItem.CreatedAt]: { field: SortListingItemField.CreatedAt, order: SortOrder.Desc },
 };
 
-export const Marketplace = () => {
-  const pageSize = 15;
+type FilterWrapperProps = {
+  totalCount?: number;
+  isGrid?: boolean;
+  genres?: GenreLabel[];
+  saleType?: SaleTypeLabel;
+  sorting: SortListingItem;
+  setSorting: Dispatch<SetStateAction<SortListingItem>>;
+  setSaleType: Dispatch<SetStateAction<SaleTypeLabel | undefined>>;
+  setGenres: Dispatch<SetStateAction<GenreLabel[] | undefined>>;
+  setIsGrid: Dispatch<SetStateAction<boolean>>;
+};
+
+const buildMarketplaceFilter = (genres: GenreLabel[] | undefined, saleType: SaleTypeLabel | undefined) => {
+  return {
+    ...(genres?.length && { genres: genres.map(genre => genre.key) }),
+    ...(saleType && { listingItem: { saleType: saleType.key } }),
+  };
+};
+
+const FilterWrapper = memo((props: FilterWrapperProps) => {
+  const { genres, saleType, sorting, setSorting, setSaleType, setGenres, isGrid, setIsGrid, totalCount = 0 } = props;
   const { dispatchShowFilterMarketplaceModal } = useModalDispatch();
-  const { genres: genresFromModal, filterSaleType } = useModalState();
-  const [isGrid, setIsGrid] = useState(true);
-  const [totalCount, setTotalCount] = useState(0);
-  const [genres, setGenres] = useState<GenreLabel[] | undefined>(undefined);
-  const [saleType, setSaleType] = useState<SaleTypeLabel | undefined>(undefined);
-  const [sorting, setSorting] = useState<SortListingItem>(SortListingItem.CreatedAt);
-  const { field, order } = SelectToApolloQuery[sorting];
-  const { data, refetch, fetchMore, loading } = useListingItemsQuery({
-    variables: {
-      page: { first: pageSize },
-      sort: { field, order },
-      filter: {},
-    },
-    ssr: false,
-  });
-
-  useEffect(() => genresFromModal && setGenres(genresFromModal), [genresFromModal]);
-  useEffect(() => filterSaleType && setSaleType(filterSaleType), [filterSaleType]);
-  useEffect(() => setTotalCount(data?.listingItems.pageInfo.totalCount || 0), [data]);
-
-  useEffect(() => {
-    refetch({
-      page: {
-        first: pageSize,
-      },
-      sort: { field, order },
-      filter: buildMarketplaceFilter(genres, saleType),
-    });
-  }, [genres, saleType, refetch, field, order]);
-
-  const loadMore = () =>
-    fetchMore({
-      variables: {
-        page: {
-          first: pageSize,
-          after: data?.listingItems.pageInfo.endCursor,
-        },
-        sort: { field, order },
-        filter: buildMarketplaceFilter(genres, saleType),
-      },
-    });
 
   return (
     <div>
@@ -82,10 +69,10 @@ export const Marketplace = () => {
           <span className="text-gray-80 text-sm">Tracks</span>
         </div>
         <button aria-label="List view">
-          <ListView color={isGrid ? undefined : 'rainbow'} onClick={() => setIsGrid(false)} />
+          <ListViewIcon color={isGrid ? undefined : 'rainbow'} onClick={() => setIsGrid(false)} />
         </button>
         <button aria-label="Grid view">
-          <GridView color={isGrid ? 'rainbow' : undefined} onClick={() => setIsGrid(true)} />
+          <GridViewIcon color={isGrid ? 'rainbow' : undefined} onClick={() => setIsGrid(true)} />
         </button>
       </div>
       <div className="flex gap-2 bg-black p-4 justify-center items-center">
@@ -114,86 +101,165 @@ export const Marketplace = () => {
           <option value={SortListingItem.CreatedAt}>Newest</option>
         </select>
       </div>
-      <div className="flex flex-wrap p-4 gap-2">
-        {saleType && (
-          <Badge
-            label={saleType.label}
-            onDelete={() => {
-              dispatchShowFilterMarketplaceModal(false, genres, undefined);
-              setSaleType(undefined);
-            }}
-          />
-        )}
-        {genres?.map(genre => (
-          <Badge
-            key={genre.key}
-            label={genre.label}
-            onDelete={() => {
-              const newGenres = genres.filter(it => it !== genre);
-              dispatchShowFilterMarketplaceModal(false, newGenres, saleType);
-              setGenres(newGenres);
-            }}
-          />
-        ))}
-      </div>
-      {loading ? (
-        isGrid ? (
-          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 p-4 justify-center">
-            <GridSkeleton />
-            <GridSkeleton />
-            <GridSkeleton />
-            <GridSkeleton />
-          </div>
-        ) : (
-          <div className="space-y-2">
-            <PostSkeleton />
-            <PostSkeleton />
-            <PostSkeleton />
-          </div>
-        )
-      ) : !data ? (
-        <NoResultFound type="items" />
-      ) : (
-        <PullToRefresh onRefresh={refetch} className="h-auto">
-          <Tracks isGrid={isGrid} tracks={data.listingItems.nodes} />
-        </PullToRefresh>
+      {(saleType || Boolean(genres?.length)) && (
+        <div className="flex flex-wrap p-4 gap-2">
+          {saleType && (
+            <Badge
+              label={saleType.label}
+              onDelete={() => {
+                dispatchShowFilterMarketplaceModal(false, genres, undefined);
+                setSaleType(undefined);
+              }}
+            />
+          )}
+          {genres?.map(genre => (
+            <Badge
+              key={genre.key}
+              label={genre.label}
+              onDelete={() => {
+                const newGenres = genres.filter(it => it !== genre);
+                dispatchShowFilterMarketplaceModal(false, newGenres, saleType);
+                setGenres(newGenres);
+              }}
+            />
+          ))}
+        </div>
       )}
-      {data?.listingItems.pageInfo.hasNextPage && (
-        <InfiniteLoader loadMore={loadMore} loadingMessage="Loading Marketplace" />
+    </div>
+  );
+});
+
+export const Marketplace = () => {
+  const pageSize = 15;
+  const { genres: genresFromModal, filterSaleType } = useModalState();
+  const [isGrid, setIsGrid] = useState(true);
+  const [genres, setGenres] = useState<GenreLabel[] | undefined>(undefined);
+  const [saleType, setSaleType] = useState<SaleTypeLabel | undefined>(undefined);
+  const [sorting, setSorting] = useState<SortListingItem>(SortListingItem.PriceAsc);
+  const { data, refetch, fetchMore, loading } = useListingItemsQuery({
+    variables: {
+      page: { first: pageSize },
+      sort: SelectToApolloQuery[sorting],
+      filter: {},
+    },
+    ssr: false,
+  });
+
+  useEffect(() => genresFromModal && setGenres(genresFromModal), [genresFromModal]);
+  useEffect(() => filterSaleType && setSaleType(filterSaleType), [filterSaleType]);
+
+  useEffect(() => {
+    refetch({
+      page: {
+        first: pageSize,
+      },
+      sort: SelectToApolloQuery[sorting],
+      filter: buildMarketplaceFilter(genres, saleType),
+    });
+  }, [genres, saleType, refetch, sorting]);
+
+  const loadMore = () => {
+    fetchMore({
+      variables: {
+        page: {
+          first: pageSize,
+          after: data?.listingItems.pageInfo.endCursor,
+        },
+        sort: SelectToApolloQuery[sorting],
+        filter: buildMarketplaceFilter(genres, saleType),
+      },
+    });
+  };
+
+  return (
+    <div>
+      <FilterWrapper
+        totalCount={data?.listingItems.pageInfo.totalCount}
+        isGrid={isGrid}
+        setIsGrid={setIsGrid}
+        genres={genres}
+        setGenres={setGenres}
+        saleType={saleType}
+        setSaleType={setSaleType}
+        sorting={sorting}
+        setSorting={setSorting}
+      />
+      {isGrid ? (
+        <GridView
+          loading={loading}
+          hasNextPage={data?.listingItems.pageInfo.hasNextPage}
+          loadMore={loadMore}
+          tracks={data?.listingItems.nodes}
+          refetch={refetch}
+        />
+      ) : (
+        <ListView
+          loading={loading}
+          hasNextPage={data?.listingItems.pageInfo.hasNextPage}
+          loadMore={loadMore}
+          tracks={data?.listingItems.nodes}
+          refetch={refetch}
+        />
       )}
     </div>
   );
 };
 
-interface TracksProps {
-  isGrid: boolean;
-  tracks: TrackWithListingItem[] | undefined;
+interface ViewProps {
+  loading: boolean;
+  loadMore: () => void;
+  refetch: () => Promise<ApolloQueryResult<ListingItemsQuery>>;
+  hasNextPage?: boolean;
+  tracks?: TrackWithListingItem[];
 }
 
-const Tracks = ({ isGrid, tracks }: TracksProps) => {
-  if (!tracks) return null;
+const ListView = ({ tracks, loading, refetch, hasNextPage, loadMore }: ViewProps) => {
   return (
     <>
-      {isGrid ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 p-4 justify-center">
-          {tracks.map(track => (
-            <TrackGrid key={track.id} track={track} />
-          ))}
-        </div>
-      ) : (
+      {loading ? (
         <div className="space-y-2">
-          {tracks.map(track => (
-            <Track key={track.id} track={track as TrackQuery['track']} />
-          ))}
+          <PostSkeleton />
+          <PostSkeleton />
+          <PostSkeleton />
         </div>
+      ) : !tracks ? (
+        <NoResultFound type="items" />
+      ) : (
+        <PullToRefresh onRefresh={refetch} className="h-auto">
+          <div className="space-y-2">
+            {tracks.map(track => (
+              <Track key={track.id} track={track as TrackQuery['track']} />
+            ))}
+          </div>
+        </PullToRefresh>
       )}
+      {hasNextPage && <InfiniteLoader loadMore={loadMore} loadingMessage="Loading Marketplace" />}
     </>
   );
 };
 
-const buildMarketplaceFilter = (genres: GenreLabel[] | undefined, saleType: SaleTypeLabel | undefined) => {
-  return {
-    ...(genres?.length && { genres: genres.map(genre => genre.key) }),
-    ...(saleType && { listingItem: { saleType: saleType.key } }),
-  };
+const GridView = ({ tracks, loading, refetch, hasNextPage, loadMore }: ViewProps) => {
+  return (
+    <>
+      {loading ? (
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 p-4 justify-center">
+          <GridSkeleton />
+          <GridSkeleton />
+          <GridSkeleton />
+          <GridSkeleton />
+        </div>
+      ) : !tracks ? (
+        <NoResultFound type="items" />
+      ) : (
+        <PullToRefresh onRefresh={refetch} className="h-auto">
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 p-4 justify-center">
+            {tracks.map(track => (
+              <TrackGrid key={track.id} track={track} />
+            ))}
+          </div>
+        </PullToRefresh>
+      )}
+      {hasNextPage && <InfiniteLoader loadMore={loadMore} loadingMessage="Loading Marketplace" />}
+    </>
+  );
 };
