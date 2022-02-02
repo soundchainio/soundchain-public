fc84122405d6a281dead5cc06b0aeebac98752dc
Mateus Batista <mateusalfredobatista@gmail.com>
2022-02-02 16:00:52 -0300

    component refactor

--- web/src/components/ExploreTracks.tsx
+++ web/src/components/ExploreTracks.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable react/display-name */
 import { Song, TrackListItem } from 'components/TrackListItem';
 import { useAudioPlayerContext } from 'hooks/useAudioPlayer';
-import { PageInput, useExploreTracksQuery } from 'lib/graphql';
+import { ExploreTracksQuery, PageInput, useExploreTracksQuery } from 'lib/graphql';
 import React, { memo } from 'react';
-import { areEqual, FixedSizeList as List } from 'react-window';
+import { areEqual, FixedSizeList as List, ListChildComponentProps } from 'react-window';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import InfiniteLoader from 'react-window-infinite-loader';
 import { NoResultFound } from './NoResultFound';
@@ -21,23 +20,6 @@ export const ExploreTracks = ({ searchTerm }: ExplorePageProps) => {
   const { data, loading, fetchMore } = useExploreTracksQuery({
     variables: { search: searchTerm, page: firstPage },
   });
-  const { playlistState } = useAudioPlayerContext();
-
-  const handleOnPlayClicked = (song: Song, index: number) => {
-    if (tracks) {
-      const list = tracks.map(
-        track =>
-          ({
-            trackId: track.id,
-            src: track.playbackUrl,
-            art: track.artworkUrl,
-            title: track.title,
-            artist: track.artist,
-          } as Song),
-      );
-      playlistState(list, index);
-    }
-  };
 
   if (loading) {
     return (
@@ -88,30 +70,7 @@ export const ExploreTracks = ({ searchTerm }: ExplorePageProps) => {
                   itemSize={56}
                   itemData={tracks}
                 >
-                  {memo(
-                    ({ data, index, style }) => (
-                      <div style={style}>
-                        {!isItemLoaded(index) ? (
-                          <LoaderAnimation loadingMessage="Loading..." />
-                        ) : (
-                          <TrackListItem
-                            song={{
-                              trackId: data[index].id,
-                              src: data[index].playbackUrl,
-                              art: data[index].artworkUrl,
-                              title: data[index].title,
-                              artist: data[index].artist,
-                              playbackCount: data[index].playbackCountFormatted,
-                              isFavorite: data[index].isFavorite,
-                            }}
-                            index={index + 1}
-                            handleOnPlayClicked={song => handleOnPlayClicked(song, index)}
-                          />
-                        )}
-                      </div>
-                    ),
-                    areEqual,
-                  )}
+                  {props => <Data currentResult={data.exploreTracks} {...props} />}
                 </List>
               )}
             </InfiniteLoader>
@@ -123,3 +82,51 @@ export const ExploreTracks = ({ searchTerm }: ExplorePageProps) => {
     </div>
   );
 };
+
+interface DataProps extends ListChildComponentProps<ExploreTracksQuery['exploreTracks']['nodes']> {
+  currentResult: ExploreTracksQuery['exploreTracks'];
+}
+
+const Data = memo(function Data({ data, index, style, currentResult }: DataProps) {
+  const { nodes: tracks, pageInfo } = currentResult;
+  const { playlistState } = useAudioPlayerContext();
+  const isItemLoaded = (index: number) => !pageInfo.hasNextPage || index < tracks.length;
+
+  const handleOnPlayClicked = (_song: Song, index: number) => {
+    if (tracks) {
+      const list = tracks.map(
+        track =>
+          ({
+            trackId: track.id,
+            src: track.playbackUrl,
+            art: track.artworkUrl,
+            title: track.title,
+            artist: track.artist,
+          } as Song),
+      );
+      playlistState(list, index);
+    }
+  };
+
+  return (
+    <div style={style}>
+      {!isItemLoaded(index) ? (
+        <LoaderAnimation loadingMessage="Loading..." />
+      ) : (
+        <TrackListItem
+          song={{
+            trackId: data[index].id,
+            src: data[index].playbackUrl,
+            art: data[index].artworkUrl,
+            title: data[index].title,
+            artist: data[index].artist,
+            playbackCount: data[index].playbackCountFormatted,
+            isFavorite: data[index].isFavorite,
+          }}
+          index={index + 1}
+          handleOnPlayClicked={song => handleOnPlayClicked(song, index)}
+        />
+      )}
+    </div>
+  );
+}, areEqual);
