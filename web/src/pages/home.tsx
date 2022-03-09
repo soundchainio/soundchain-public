f945d471a323fc5fe951c46648296f71a2ebcbe1
Leonardo Cavalcanti <leonardo.cavalcanti91@hotmail.com>
2022-03-09 18:55:14 -0300

    set marketplace as the initial page

---
+++ web/src/pages/home.tsx
@@ -0,0 +1,47 @@
+import { InboxButton } from 'components/Buttons/InboxButton';
+import { Feed } from 'components/Feed';
+import { Posts } from 'components/Posts';
+import SEO from 'components/SEO';
+import { TopNavBarProps } from 'components/TopNavBar';
+import { useLayoutContext } from 'hooks/useLayoutContext';
+import { cacheFor, createApolloClient } from 'lib/apollo';
+import { MeDocument, MeQuery } from 'lib/graphql';
+import { GetServerSideProps } from 'next';
+import { useEffect, useMemo } from 'react';
+
+interface HomePageProps {
+  me?: MeQuery['me'];
+}
+
+export const getServerSideProps: GetServerSideProps<HomePageProps> = async context => {
+  const apolloClient = createApolloClient(context);
+
+  const { data } = await apolloClient.query({
+    query: MeDocument,
+    context,
+  });
+
+  return cacheFor(HomePage, { me: data.me }, context, apolloClient);
+};
+
+export default function HomePage({ me }: HomePageProps) {
+  const { setTopNavBarProps } = useLayoutContext();
+
+  const topNavBarProps: TopNavBarProps = useMemo(
+    () => ({
+      rightButton: me ? <InboxButton /> : undefined,
+    }),
+    [me],
+  );
+
+  useEffect(() => {
+    setTopNavBarProps(topNavBarProps);
+  }, [setTopNavBarProps, topNavBarProps]);
+
+  return (
+    <>
+      <SEO title="SoundChain" description="Connecting people to music" canonicalUrl="/" />
+      <div className="pt-3 h-full">{me ? <Feed /> : <Posts />}</div>
+    </>
+  );
+}
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,47 +1,14 @@
-import { InboxButton } from 'components/Buttons/InboxButton';
-import { Feed } from 'components/Feed';
-import { Posts } from 'components/Posts';
-import SEO from 'components/SEO';
-import { TopNavBarProps } from 'components/TopNavBar';
-import { useLayoutContext } from 'hooks/useLayoutContext';
-import { cacheFor, createApolloClient } from 'lib/apollo';
-import { MeDocument, MeQuery } from 'lib/graphql';
-import { GetServerSideProps } from 'next';
-import { useEffect, useMemo } from 'react';
+import { useRouter } from 'next/router';
+import { useMe } from 'hooks/useMe';
+import { useEffect } from 'react';
 
-interface HomePageProps {
-  me?: MeQuery['me'];
-}
-
-export const getServerSideProps: GetServerSideProps<HomePageProps> = async context => {
-  const apolloClient = createApolloClient(context);
-
-  const { data } = await apolloClient.query({
-    query: MeDocument,
-    context,
-  });
-
-  return cacheFor(HomePage, { me: data.me }, context, apolloClient);
-};
-
-export default function HomePage({ me }: HomePageProps) {
-  const { setTopNavBarProps } = useLayoutContext();
-
-  const topNavBarProps: TopNavBarProps = useMemo(
-    () => ({
-      rightButton: me ? <InboxButton /> : undefined,
-    }),
-    [me],
-  );
+export default function Index() {
+  const router = useRouter();
+  const me = useMe();
 
   useEffect(() => {
-    setTopNavBarProps(topNavBarProps);
-  }, [setTopNavBarProps, topNavBarProps]);
+    router.push(me ? '/marketplace' : '/home');
+  }, [me]);
 
-  return (
-    <>
-      <SEO title="SoundChain" description="Connecting people to music" canonicalUrl="/" />
-      <div className="pt-3 h-full">{me ? <Feed /> : <Posts />}</div>
-    </>
-  );
+  return null;
 }
