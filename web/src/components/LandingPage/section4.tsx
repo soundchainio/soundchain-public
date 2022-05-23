e76481062297919b480562ff8958fd3ec7f4c782
Reginaldo Junior <reginaldo.junior696@gmail.com>
2022-05-23 16:42:31 -0300

    feat: added roadmap

---
+++ web/src/components/LandingPage/section4.tsx
@@ -0,0 +1,46 @@
+import { twd } from '../utils/tw';
+import Image from 'next/image';
+
+const orngBash = twd(`text-transparent bg-clip-text bg-gradient-to-b 
+from-[#FED503] to-[#FE5540]`);
+
+const OrngText = orngBash.span;
+const OrngCol = orngBash(`flex flex-col bg-none bg-[#FED503]`).div;
+
+export function Section4() {
+  return (
+    <div className='relative'>
+      <div className='max-w-7xl lg:max-w-full mx-auto sm:px-6 lg:px-0 min-h-[500px]
+             vsm:min-h-[600px] vmd:min-h-[750px] vlg:min-h-[900px] vxl:min-h-[1100px] v2xl:min-h-[1400px]
+              max-h-screen flex flex-col'>
+        <div className='container mx-auto h-full flex-1 xl:px-40'>
+          <div className='relative flex flex-col items-start h-full pl-4 py-16 sm:pl-6 sm:py-24 lg:py-32 lg:pl-8'>
+            <div className='text-center w-full flex flex-col relative'>
+              <OrngText className='text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold'>collect</OrngText>
+              <span className='text-3xl'>exclusive music NFTs</span>
+            </div>
+            <div className='mt-20 flex items-center'>
+
+              <div className='mt-10 flex flex-col min-w-[400px]'>
+                <span className='text-white font-semibold text-3xl'>Full ownership over how</span>
+                <OrngCol className='text-7xl uppercase'>
+                  <span>your</span>
+                  <span>art</span>
+                </OrngCol>
+                <span className='text-white font-semibold text-3xl'>is used</span>
+              </div>
+              <div className='max-w-[584px]'>
+                <Image
+                  layout='intrinsic'
+                  width='584px'
+                  height='544px'
+                  className='h-full w-full object-contain'
+                  src='/landing-page/laptop.png' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ web/src/components/LandingPage/section5.tsx
@@ -0,0 +1,69 @@
+import { twd } from '../utils/tw';
+import { ArrowRightIcon } from '@heroicons/react/solid';
+import { Disclosure } from '@headlessui/react';
+import { Button } from '../Button';
+
+const orngBash = twd(`text-transparent bg-clip-text bg-gradient-to-b 
+from-[#FED503] to-[#FE5540]`);
+
+const OrngText = orngBash.span;
+
+export function Section5() {
+  return (
+    <div className='relative'>
+      <div className='max-w-7xl lg:max-w-full
+       min-h-[500px] vsm:min-h-[600px] vmd:min-h-[750px] vlg:min-h-[900px] vxl:min-h-[1100px] v2xl:min-h-[1400px]
+              max-h-screen flex flex-col'>
+        <div className='mx-auto h-full flex-1'>
+          <div className='relative flex flex-col items-start h-full sm:py-24 lg:py-32'>
+            <div className='text-center w-full flex flex-col relative'>
+              <OrngText className='text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold'>What is next?</OrngText>
+            </div>
+            <div className='mt-20 flex flex-col w-full max-w-15/16 md:max-w-full w-screen mx-auto pr-2 md:max-w-screen 2k:max-w-[1200px]'>
+              <Disclosure>
+                <Disclosure.Button className='flex items-center py-4 px-10 text-black bg-[#FE5540]'>
+                  <h2 className='font-bold text-6xl uppercase'>UI/UX updates</h2>
+                  <div className='flex-1' />
+                  <span className='font-semibold text-xl'>In progress</span>
+                  <ArrowRightIcon className='w-6 h-6 ml-4' />
+                </Disclosure.Button>
+                <Disclosure.Panel className='border-[#FE5540] border-x-4 py-4 px-4'>
+                  some intel about UI/UX
+                </Disclosure.Panel>
+              </Disclosure>
+              <Disclosure>
+                <Disclosure.Button className='flex items-center py-4 px-10 text-black bg-[#FED503]'>
+                  <h2 className='font-bold text-6xl uppercase'>Token Launch</h2>
+                  <div className='flex-1' />
+                  <span className='font-semibold text-xl'>Jun 21, 2022</span>
+                  <ArrowRightIcon className='w-6 h-6 ml-4' />
+                </Disclosure.Button>
+                <Disclosure.Panel className='border-[#FED503] border-x-4 py-4 px-4'>
+                  some intel about the token launch
+                </Disclosure.Panel>
+              </Disclosure>
+              <Disclosure>
+                <Disclosure.Button className='flex items-center py-4 px-10 text-black bg-[#AC4EFD]'>
+                  <h2 className='font-bold text-6xl uppercase'>Multi-Edition NFTs</h2>
+                  <div className='flex-1' />
+                  <span className='font-semibold text-xl'>July/August</span>
+                  <ArrowRightIcon className='w-6 h-6 ml-4' />
+                </Disclosure.Button>
+                <Disclosure.Panel className='border-[#AC4EFD] border-4 py-4 px-4'>
+                  some intel about the multi-edition nfts
+                </Disclosure.Panel>
+              </Disclosure>
+            </div>
+            <div className='mt-10 flex justify-center w-full'>
+              <Button variant='rainbow' className='rounded-lg' onClick={() => {
+                alert('viewing full roadmap');
+              }}>
+                <span className='font-medium px-6 uppercase'>See full roadmap</span>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
