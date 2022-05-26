475aa62a23d03b6d47027c98cf98b8192fba88be
Marcos <flavouredsoda@gmail.com>
2022-05-26 11:06:34 -0300

    Fix: Header css

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -7,31 +7,42 @@ interface NavItemProps {
   currentPage: string;
 }
 
-export const NavItem = ({text, currentPage}: NavItemProps) => {
+export const NavItem = ({ text }: NavItemProps) => {
+  const router = useRouter();
+  const currentPage = router.pathname.replace("/", "");
+  const underline = currentPage.toLowerCase() === text.toLowerCase();
 
-  const underline = (currentPage.toLowerCase() === text.toLowerCase() ?
-  "" : "") 
-
-  return <div className={'flex justify-center items-center p-2' + underline}>
-    {text}
-  </div>
-}
+  return underline ? 
+    (<div className="flex flex-col items-center">
+        <div 
+          className='flex items-center justify-center p-2'>{text}
+        </div>
+      <div className="bg-green-blue-gradient w-5/6 pb-1"/>
+    </div>)
+    :
+    (<a href={"/" + text.toLowerCase()}>
+      <div className='flex items-center justify-center p-2'>{text}</div>
+     </a>)
+};
 
 export const Header = () => {
   const router = useRouter();
 
   return (
     <header className="h-20">
-      <div className="w-full h-20 my-0 flex items-center justify-between px-6 pt-4 lg:px-10">
-        <div className='flex items-center'>
+      <div className="my-0 flex h-20 w-full items-center justify-between px-6 pt-4 lg:px-10">
+        <div className="flex items-center">
           <Logo className="h-[50px]" />
-          <span className="text-white font-extrabold hidden md:block">SoundChain</span>
+          <span className="hidden font-extrabold text-white md:block">SoundChain</span>
         </div>
-        <nav className='text-white flex gap-3 justify-evenly items-center'>
-          <Button className='h-12 w-96' variant="rainbow" onClick={() => router.push('/marketplace')}>
+        <nav className="flex items-center justify-evenly gap-3 text-white">
+          <NavItem text="Tokenomics" />
+          <NavItem text="Airdrop" />
+          <NavItem text="Stake" />
+          <Button className="h-12 w-96" variant="rainbow" onClick={() => router.push('/marketplace')}>
             CONTINUE TO SOUNDCHAIN
           </Button>
-          </nav>
+        </nav>
       </div>
     </header>
   );
