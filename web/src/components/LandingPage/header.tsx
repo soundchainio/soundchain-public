d336568dcc924f2959557633360c1a5dd4d94bfa
Reginaldo Junior <reginaldo.junior696@gmail.com>
2022-06-03 10:59:09 -0300

    fix: added mobile drawer to header

--- web/src/components/LandingPage/header.tsx
+++ web/src/components/LandingPage/header.tsx
@@ -1,30 +1,80 @@
 import { Logo } from '../../icons/Logo';
 import Link from 'next/link';
+import { Dialog, Transition } from '@headlessui/react';
+import { MenuIcon } from '@heroicons/react/solid';
+import React, { useState } from 'react';
+
+function HeaderDrawer({ open, close }: { open: boolean, close: () => void }) {
+  return (
+    <Transition
+      show={open}
+      as={React.Fragment}>
+      <Dialog onClose={close} className='relative z-50'>
+        <div className='fixed inset-0 flex items-center justify-end p-0 bg-black/50'>
+          <Transition.Child
+            as={React.Fragment}
+            enter='transition duration-200 ease-out'
+            enterFrom='transform translate-x-[100%]'
+            enterTo='transform translate-x-0 opacity-100'
+            leave='transition duration-100 ease-out'
+            leaveFrom='transform translate-x-0 opacity-100'
+            leaveTo='transform translate-x-[100%]'>
+            <Dialog.Panel className='w-full h-screen max-w-[300px] rounded bg-[#131313]'>
+              <Dialog.Description className='p-4'>
+                <ul className='gap-4 flex flex-col'>
+                  <li className='font-semibold text-md text-slate-400 hover:text-slate-200 cursor-pointer'>
+                    <Link href='/roadmap'>
+                      <a>Roadmap</a>
+                    </Link>
+                  </li>
+                  <li className='font-semibold text-md text-slate-400 hover:text-slate-200 cursor-pointer'>
+                    <Link href='/ogun'>
+                      <a>OGUN Token</a>
+                    </Link>
+                  </li>
+                </ul>
+              </Dialog.Description>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
 
 export default function LandingPageHeader() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
-    <header className='h-14'>
-      <nav className='flex container mx-auto items-center text-white h-full px-4 md:px-0'>
-        <div className='flex items-center gap-4'>
-          <Logo className='block h-8 w-auto' />
-          <Link href='/'>
-            <a className='text-slate-50 text-lg font-semibold'>SoundChain</a>
-          </Link>
-        </div>
-        <div className='flex-1' />
-        <ul className='flex gap-4'>
-          <li className='font-semibold text-md text-slate-400 hover:text-slate-200 cursor-pointer'>
-            <Link href='#roadmap'>
-              <a>Roadmap</a>
+    <>
+      <header className='h-14'>
+        <nav className='flex container mx-auto items-center text-white h-full px-4 md:px-0'>
+          <div className='flex items-center gap-4'>
+            <Logo className='block h-8 w-auto' />
+            <Link href='/'>
+              <a className='text-slate-50 text-lg font-semibold'>SoundChain</a>
             </Link>
-          </li>
-          <li className='font-semibold text-md text-slate-400 hover:text-slate-200 cursor-pointer'>
-            <Link href='/ogun' target='_blank'>
-              <a>OGUN Token</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
+          </div>
+          <div className='flex-1' />
+          <ul className='gap-4 hidden md:flex'>
+            <li className='font-semibold text-md text-slate-400 hover:text-slate-200 cursor-pointer'>
+              <Link href='#roadmap'>
+                <a>Roadmap</a>
+              </Link>
+            </li>
+            <li className='font-semibold text-md text-slate-400 hover:text-slate-200 cursor-pointer'>
+              <Link href='/ogun' target='_blank'>
+                <a>OGUN Token</a>
+              </Link>
+            </li>
+          </ul>
+          <button onClick={() => setDrawerOpen(true)} className='outline-0 active:outline-0 focus:outline-0 p-4'>
+            <MenuIcon className='w-6 h-6 text-white md:hidden' />
+          </button>
+        </nav>
+      </header>
+
+      <HeaderDrawer open={drawerOpen} close={() => setDrawerOpen(false)} />
+    </>
+  );
 }
--- web/src/components/roadmap/layout.tsx
+++ web/src/components/roadmap/layout.tsx
@@ -11,26 +11,30 @@ interface RoadmapLayoutProps {
 export default function RoadmapLayout(props: RoadmapLayoutProps) {
   return (
     <div className='relative h-full flex bg-black flex-col text-white overflow-x-hidden font-rubik'>
-      <LandingPageHeader />
+      <div className='relative flex flex-col bg-[#131313]'>
+        <LandingPageHeader />
+      </div>
 
-      <div className='container'>
-        <div className='relative'>
+      <div className='lg:container'>
+        <div className='relative w-full h-[400px] sm:h-[500px] md:h-[600px] lg:h-[700]'>
           <Image
             src='/landing-page/roadmap-background.svg'
             width={1488}
             height={952}
-            layout='responsive'
+            layout='fill'
+            className='object-cover '
           />
 
+
           <div className='absolute inset-0 w-full h-full flex items-center justify-center'>
-            <span className='text-6xl font-bold text-center'>SOUNDCHAIN<br />ROADMAP</span>
+            <span className='text-3xl sm:text-4xl md:text-6xl font-bold text-center'>SOUNDCHAIN<br />ROADMAP</span>
           </div>
         </div>
       </div>
 
 
       <div className='relative'>
-        <div className={`mt-20 flex flex-col w-full max-w-15/16 md:max-w-full w-screen mx-auto 
+        <div className={` flex flex-col w-full max-w-15/16 md:max-w-full w-screen mx-auto 
         sm:pr-2 md:max-w-screen 2k:max-w-[1200px] pb-10 md:pb-20`}>
           <RoadmapStepList steps={roadmapSteps} />
         </div>
