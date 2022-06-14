e57687c22d70204338d15c14bd73fd55e03411a6
Reginaldo Junior <reginaldo.junior696@gmail.com>
2022-06-14 15:08:06 -0300

    fix: fixed discover section layout on small and medium screens

--- web/src/components/LandingPage/discoverTracksSection.tsx
+++ web/src/components/LandingPage/discoverTracksSection.tsx
@@ -29,26 +29,26 @@ export function DiscoverTracksSection() {
         </div>
       </div>
 
-      <div className='relative z-20'>
-        <SectionWrapper className=''>
+      <div className='relative z-20 sm:pt-64 sm:pb-64 md:pt-96 md:pb-20'>
+        <SectionWrapper className='mt-20 md:mt-0'>
           <div className='container mx-auto h-full flex-1 xl:px-40'>
-            <div className='relative flex flex-col items-start h-full pl-4 py-16 md:pl-6 md:py-24 lg:py-32 lg:pl-8'>
-              <div className='text-center w-full flex flex-col relative'>
+            <div className='relative flex flex-col items-start h-full pl-4 py-16 md:pl-6 sm:py-24 md:py-32 md:pl-8'>
+              <div className='text-center w-full flex flex-col relative mt-20 lg:mt-0'>
                 <BlueishText className='text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold'>discover</BlueishText>
                 <span className='text-3xl'>high fi tracks</span>
               </div>
               <div className='mt-20 flex w-full'>
                 <div className='relative flex justify-center mx-auto'>
                   <div className='absolute inset-0'>
-                    <div className='w-full h-full absolute inset-0 flex items-center justify-center rounded-xl z-0 bg-gradient-to-t sm:bg-gradient-to-r from-white/50 to-transparent scale-[102%]'>
+                    <div className='w-full h-full absolute inset-0 flex items-center justify-center rounded-xl z-0 bg-gradient-to-t md:bg-gradient-to-r from-white/50 to-transparent scale-[102%]'>
                       <div className='w-[98%] mx-auto my-auto h-[98%] rounded-xl  bg-gradient-to-t md:bg-gradient-to-r from-slate-600/50 to-transparent p-1'>
 
                       </div>
                     </div>
                   </div>
-                  <div className={`mx:auto w-[360px] sm:w-[400px] md:w-[700px] flex flex-col items-start
-                  pt-[23rem] sm:pt-10 sm:h-[360px] sm:pl-10 relative rounded-xl bg-gradient-to-t sm:bg-gradient-to-r from-slate-900/75 to-transparent`}>
-                    <div className='flex items-center gap-4 2xl:ml-10 w-full justify-center sm:justify-start'>
+                  <div className={`mx:auto w-[360px] md:w-[400px] md:w-[700px] flex flex-col items-start
+                  pt-[23rem] md:pt-10 md:h-[360px] md:pl-10 relative rounded-xl bg-gradient-to-t md:bg-gradient-to-r from-slate-900/75 to-transparent`}>
+                    <div className='flex items-center gap-4 2xl:ml-10 w-full justify-center md:justify-start'>
                       <span>created</span>
                       <BlueishCol className='text-4xl font-bold'>
                         <span>by</span>
@@ -57,14 +57,14 @@ export function DiscoverTracksSection() {
                       <span>artists</span>
                     </div>
 
-                    <span className='max-w-7/10 mt-4 text-center mx-auto sm:mx-0 mb-10 sm:mb-0'>
+                    <span className='max-w-7/10 mt-4 text-center mx-auto md:mx-0 mb-10 md:mb-0'>
                     We support and connect artists like never before with tools to actively engage with fans,
                     and nearly 100% of profits going directly to artists.
                   </span>
                   </div>
 
                   <div
-                    className='absolute pointer-events-none left-0 right-0 -top-[4rem] mx-auto sm:left-auto max-w-[190px] sm:-right-20  sm:-top-10'>
+                    className='absolute pointer-events-none left-0 right-0 -top-[4rem] mx-auto md:left-auto max-w-[190px] md:-right-20  md:-top-10'>
                     <Image
                       alt=''
                       src='/landing-page/timeline-preview.png'
