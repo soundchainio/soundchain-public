38ea1b190bc9d49874b4153978aacf84dadc1840
Reginaldo Junior <reginaldo.junior696@gmail.com>
2022-06-17 10:59:30 -0300

    feat: added social links to landing page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,8 @@
 import SEO from 'components/SEO';
+import { Discord } from 'icons/social/Discord';
+import { Instagram } from 'icons/social/Instagram';
+import { Twitter } from 'icons/social/Twitter';
+import { YoutubeBW } from 'icons/social/YoutubeBW';
 import type { ReactElement } from 'react';
 import LandingPageLayout from '../components/LandingPage/layout';
 
@@ -7,9 +11,23 @@ export default function Index() {
     <>
       <SEO title='SoundChain' description='SoundChain' canonicalUrl='/' />
 
-      <footer className='bg-[#131313] text-[#505050] w-full mt-4 md:mt-10 lg:mt-0'>
-        <div className='h-[60px] container mx-auto flex items-center justify-center'>
+      <footer className='bg-[#131313] text-[#505050] w-full mt-4 md:mt-16 lg:mt-12'>
+        <div className='h-[60px] container mx-auto flex flex-col-reverse md:flex-row items-center justify-center'>
           <span className='font-bold'>SoundChain. {new Date().getFullYear()} - MADE BY AE.STUDIO</span>
+          <div className='flex items-center justify-end gap-3 px-3 justify-self-end lg:ml-3'>
+            <a  href='https://instagram.com/soundchain.io' target="_blank" rel="noreferrer">
+              <Instagram width={18} height={18} />
+            </a>
+            <a  href='https://twitter.com/soundchain_io' target="_blank" rel="noreferrer">
+              <Twitter width={18} height={18} />
+            </a>
+            <a  href='https://youtube.com/channel/UC-TJ1KIYWCYLtngwaELgyLQ' target="_blank" rel="noreferrer">
+              <YoutubeBW width={22} height={22} />
+            </a>
+            <a href='https://discord.gg/rDxev3QSGg' target="_blank" rel="noreferrer">
+              <Discord width={18} height={18} />
+            </a>
+          </div>
         </div>
       </footer>
     </>
