12730b57a9f785cff50d155abc7d1a31b4396e70
Felipe Taboada <fliptaboada@gmail.com>
2022-07-15 09:28:45 -0300

    adding contract metadata endpoint

---
+++ api/src/lambda/contractMetadata.ts
@@ -0,0 +1,10 @@
+import type { Handler } from 'aws-lambda';
+
+export const contractMetadata: Handler = async (...args) => {
+  return {
+    name: "SoundChain",
+    description: "SoundChain is a decentralized music platform that allows users to share their music with the world.",
+    image: "https://soundchain.io/soundchain-app-icon.png",
+    external_link: "https://soundchain.io",
+  }
+};
