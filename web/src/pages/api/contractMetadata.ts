5f8a345ebc35b7cc5bf79e5f6f9c7a8231c15fb5
Felipe Taboada <fliptaboada@gmail.com>
2022-07-15 10:44:44 -0300

    moving metadata to next

--- web/src/pages/api/contractMetadata.ts
+++ web/src/pages/api/contractMetadata.ts
@@ -1,10 +1,14 @@
-import type { Handler } from 'aws-lambda';
-
-export const contractMetadata: Handler = async () => {
-  return {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const handler = (req: any, res: any) => {
+  const metadata = {
     name: "SoundChain",
     description: "SoundChain is a decentralized music platform that allows users to share their music with the world.",
     image: "https://soundchain.io/soundchain-app-icon.png",
     external_link: "https://soundchain.io",
   }
-};
+
+  return res.status(200).send(metadata)
+}
+
+
+export default handler
