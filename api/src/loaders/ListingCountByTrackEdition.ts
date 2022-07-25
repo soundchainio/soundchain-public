d42bdf2184efa3de4c30da254884236f7cca1bc2
Reginaldo Junior <reginaldo.junior696@gmail.com>
2022-07-25 11:35:22 -0300

    fix mongo query to group on db instead of in-memory

--- api/src/loaders/ListingCountByTrackEdition.ts
+++ api/src/loaders/ListingCountByTrackEdition.ts
@@ -1,15 +1,28 @@
 import DataLoader from 'dataloader';
 import { BuyNowItemModel } from '../models/BuyNowItem';
-import { groupBy, mapValues } from 'lodash';
+import { groupBy, keyBy, mapValues } from 'lodash';
 
 export const ListingCountByTrackEdition = () => {
   return new DataLoader<string, number>(async (keys: readonly string[]) => {
-    const entities = await BuyNowItemModel.find({
-      trackEditionId: {
-        $in: keys
+    const keyStrings = keys.map(key => String(key))
+    const entities = await BuyNowItemModel.aggregate([
+      {
+        $match: {
+          trackEditionId: {
+            $in: keyStrings
+          }
+        }
+      },
+      {
+        $group: {
+          _id: "$trackEditionId",
+          total: {
+            $sum: 1
+          }
+        }
       }
-    })
-    const grouped = mapValues(groupBy(entities, 'trackEditionId'), group => group.length);
+    ])
+    const grouped = mapValues(keyBy(entities, '_id'), group => group.total);
     return keys.map(key => grouped[key] || 0)
   })
 }
