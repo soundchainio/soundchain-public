eb7b3dc9a58ef4a82e96fd4e9c3c236b8684d6c9
Filipe Alcantara <cauepor@gmail.com>
2022-08-22 16:00:04 -0300

    feat(#183051272): add an specific section to show the ogun bonus that should be received on listing or buying an NFT

--- web/src/components/details-NFT/BuyNow.tsx
+++ web/src/components/details-NFT/BuyNow.tsx
@@ -1,7 +1,10 @@
 import MaxGasFee from 'components/MaxGasFee';
 import { SoundchainFee } from 'components/SoundchainFee';
 import { WalletSelector } from 'components/WalletSelector';
-import React from 'react';
+import useBlockchainV2 from 'hooks/useBlockchainV2';
+import { useWalletContext } from 'hooks/useWalletContext';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
 
 interface BuyNowProps {
   price: number;
@@ -14,6 +17,19 @@ interface BuyNowProps {
 export const BuyNow = ({ price, priceOGUN, isPaymentOGUN, ownerAddressAccount, startTime }: BuyNowProps) => {
   const hasStarted = startTime <= new Date().getTime() / 1000;
 
+  const { web3 } = useWalletContext();
+  const { getRewardsRate } = useBlockchainV2();
+  const [rewardRatePercentage, setRewardsRatePercentage] = useState('');
+
+  useEffect(() => {
+    const fetchRewardRate = async () => {
+      const result1e4 = await getRewardsRate(web3 as Web3);
+      const calculatedPercentage = (parseInt(result1e4) / 10000) * 100;
+      setRewardsRatePercentage(calculatedPercentage.toString());
+    };
+    fetchRewardRate();
+  }, [setRewardsRatePercentage, getRewardsRate, web3]);
+
   if (!hasStarted) {
     return null;
   }
@@ -23,7 +39,11 @@ export const BuyNow = ({ price, priceOGUN, isPaymentOGUN, ownerAddressAccount, s
       <WalletSelector className="bg-gray-10 py-2" ownerAddressAccount={ownerAddressAccount} />
 
       <div className="flex flex-col gap-4 bg-gray-20 px-4 py-6">
-        <SoundchainFee price={isPaymentOGUN ? priceOGUN : price} isPaymentOGUN={isPaymentOGUN} />
+        <SoundchainFee
+          price={isPaymentOGUN ? priceOGUN : price}
+          isPaymentOGUN={isPaymentOGUN}
+          rewardRatePercentage={rewardRatePercentage}
+        />
         <MaxGasFee />
       </div>
     </div>
