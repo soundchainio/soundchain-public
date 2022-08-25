6272e07d1cd070c3b483e7d2101d00a823b6bb83
Randal Andrade Nunes <randal.andrade@hotmail.com>
2022-08-25 19:01:00 -0300

    feat: sentry service

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import '@reach/dialog/styles.css';
 import '@reach/slider/styles.css';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import { CheckBodyScroll } from 'components/CheckBodyScroll';
 import { Layout } from 'components/Layout';
-import { config } from 'config';
 import { StateProvider } from 'contexts';
 import { AudioPlayerProvider } from 'hooks/useAudioPlayer';
 import { HideBottomNavBarProvider } from 'hooks/useHideBottomNavBar';
@@ -18,6 +15,7 @@ import Script from 'next/script';
 import NProgress from 'nprogress';
 import { ReactElement } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
+import { sentryInitializer } from 'services/sentry';
 import 'styles/audio-player.css';
 import 'styles/bottom-audio-player.css';
 import 'styles/globals.css';
@@ -29,52 +27,48 @@ import 'styles/volume-slider.css';
 const WalletProvider = dynamic(import('hooks/useWalletContext'));
 const MagicProvider = dynamic(import('hooks/useMagicContext'));
 
-Sentry.init({
-  dsn: config.sentryUrl,
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0,
-  environment: `${process.env.NEXT_PUBLIC_VERCEL_ENV}`,
-});
-
 NProgress.configure({
   showSpinner: false,
 });
 
+sentryInitializer();
 Router.events.on('routeChangeStart', NProgress.start);
 Router.events.on('routeChangeComplete', NProgress.done);
 Router.events.on('routeChangeError', NProgress.done);
 
-export type CustomLayout = (page: ReactElement) => ReactElement
+export type CustomLayout = (page: ReactElement) => ReactElement;
 
 interface CustomAppProps extends Pick<AppProps, 'Component' | 'pageProps'> {
   Component: AppProps['Component'] & {
-    getLayout?: CustomLayout
+    getLayout?: CustomLayout;
   };
 }
 
 function SoundchainMainLayout({ Component, pageProps }: CustomAppProps) {
-  return <ApolloProvider pageProps={pageProps}>
-    <StateProvider>
-      <MagicProvider>
-        <WalletProvider>
-          <AudioPlayerProvider>
-            <HideBottomNavBarProvider>
-              <LayoutContextProvider>
-                <CheckBodyScroll />
-                <Layout>
-                  <Component {...pageProps} />
-                </Layout>
-              </LayoutContextProvider>
-            </HideBottomNavBarProvider>
-          </AudioPlayerProvider>
-        </WalletProvider>
-      </MagicProvider>
-    </StateProvider>
-  </ApolloProvider>;
+  return (
+    <ApolloProvider pageProps={pageProps}>
+      <StateProvider>
+        <MagicProvider>
+          <WalletProvider>
+            <AudioPlayerProvider>
+              <HideBottomNavBarProvider>
+                <LayoutContextProvider>
+                  <CheckBodyScroll />
+                  <Layout>
+                    <Component {...pageProps} />
+                  </Layout>
+                </LayoutContextProvider>
+              </HideBottomNavBarProvider>
+            </AudioPlayerProvider>
+          </WalletProvider>
+        </MagicProvider>
+      </StateProvider>
+    </ApolloProvider>
+  );
 }
 
 function SoundchainPageLayout({ Component, pageProps }: CustomAppProps) {
-  if (!Component.getLayout) return <></>
+  if (!Component.getLayout) return <></>;
 
   return Component.getLayout(<Component {...pageProps} />);
 }
@@ -83,23 +77,23 @@ function SoundchainApp({ Component, pageProps }: CustomAppProps) {
   return (
     <>
       <Head>
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover' />
-        <meta name='theme-color' content='#000000' />
-        <link rel='icon' href='/favicons/favicon.ico' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
-        <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#5bbad5' />
-        <meta name='msapplication-TileColor' content='#da532c' />
-        <link rel='manifest' href='/manifest.json'></link>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <link rel="manifest" href="/manifest.json"></link>
         <Script
           async
-          strategy='afterInteractive'
+          strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
         />
         <Script
-          strategy='afterInteractive'
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
--- web/src/utils/errorHandler.ts
+++ web/src/utils/errorHandler.ts
@@ -1,8 +1,32 @@
-import * as Sentry from "@sentry/react";
+import { captureException } from '@sentry/react';
+import { Event } from '@sentry/types';
+interface KeyValue {
+    [key: string]: unknown
+}
+interface SentryErrorHandlerParams {
+  error: InstanceType<ErrorConstructor>;
+  tags?: KeyValue;
+}
 
 export const errorHandler = (error: unknown) => {
-    const errorMessage = error.message
-
-    console.error(errorMessage || error)
-    Sentry.captureException(error);
+    if (error instanceof Error) console.error(error.message)
+    
+    captureException(error);
 }
+
+
+export const sentryDispatchManualError = (params: SentryErrorHandlerParams) => {
+  if (process.env.NODE_ENV !== 'production') return;
+
+  const { error, tags } = params;
+
+  captureException(error, {
+    tags: { ...tags, manuallyDispatched: true },
+  });
+};
+
+export const sentryErrorFilter = (event: Event) => {
+  if (event.tags?.manuallyDispatched) return null;
+
+  return event;
+};
