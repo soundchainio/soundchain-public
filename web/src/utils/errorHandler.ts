13c18fad27297bc2951d090ae1b55665f938dd1f
Randal Andrade Nunes <randal.andrade@hotmail.com>
2022-08-25 22:16:29 -0300

    fix: PR issues

--- web/src/utils/errorHandler.ts
+++ web/src/utils/errorHandler.ts
@@ -1,32 +1,7 @@
 import { captureException } from '@sentry/react';
-import { Event } from '@sentry/types';
-interface KeyValue {
-    [key: string]: unknown
-}
-interface SentryErrorHandlerParams {
-  error: InstanceType<ErrorConstructor>;
-  tags?: KeyValue;
-}
 
 export const errorHandler = (error: unknown) => {
-    if (error instanceof Error) console.error(error.message)
-    
-    captureException(error);
+  if (error instanceof Error) console.error(error.message)
+  
+  captureException(error);
 }
-
-
-export const sentryDispatchManualError = (params: SentryErrorHandlerParams) => {
-  if (process.env.NODE_ENV !== 'production') return;
-
-  const { error, tags } = params;
-
-  captureException(error, {
-    tags: { ...tags, manuallyDispatched: true },
-  });
-};
-
-export const sentryErrorFilter = (event: Event) => {
-  if (event.tags?.manuallyDispatched) return null;
-
-  return event;
-};
