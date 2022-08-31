27a5d7260e777b6721f9b1d53454e7c4f53d5f90
Felipe Taboada <fliptaboada@gmail.com>
2022-08-31 16:39:07 -0700

    multiple contract support for auctions

--- web/src/pages/tracks/[id]/cancel-auction.tsx
+++ web/src/pages/tracks/[id]/cancel-auction.tsx
@@ -111,7 +111,10 @@ export default function CompleteAuctionPage({ track, auctionItem }: TrackPagePro
     }
     setLoading(true)
 
-    cancelAuction(auctionItem.tokenId, account, { nft: track.nftData?.contract })
+    cancelAuction(auctionItem.tokenId, account, {
+      nft: track.nftData?.contract,
+      auction: auctionItem.contract,
+    })
       .onReceipt(() => onReceipt)
       .onError(cause => toast.error(cause.message))
       .finally(() => setLoading(false))
