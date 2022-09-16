7c01e6b8e7cd22f7358b3dc85c88fcc505f13a1d
Randal Andrade Nunes <randal.andrade@hotmail.com>
2022-09-16 11:48:32 -0300

    feat: add login button to mobile menu

--- web/src/components/LandingPage/Header/drawer.tsx
+++ web/src/components/LandingPage/Header/drawer.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import { Dialog, Transition } from '@headlessui/react'
 import { Logo } from '../../../icons/Logo'
+import { Button } from 'components/Button'
 
 export const HeaderDrawer = ({ open, close }: { open: boolean; close: () => void }) => {
   return (
@@ -19,7 +20,7 @@ export const HeaderDrawer = ({ open, close }: { open: boolean; close: () => void
           >
             <Dialog.Panel className="h-screen w-full max-w-[300px] rounded bg-black">
               <Dialog.Description>
-                <div className="mt-6 flex flex-col">
+                <div className="mt-4 flex flex-col">
                   <div className="flex w-full items-center justify-center border-b-[1px] border-gray-30 pb-6">
                     <Logo className="block h-8 w-auto" />
                   </div>
@@ -46,8 +47,20 @@ export const HeaderDrawer = ({ open, close }: { open: boolean; close: () => void
                     </li>
                     <li className="text-md mb-4 cursor-pointer border-b-[1px] border-gray-30 bg-gradient-to-r from-[#ab4eff] to-[#84ff82] bg-clip-text pl-6 pb-4 font-normal text-gray-80 transition duration-150 ease-in-out hover:text-transparent">
                       <Link href="https://soundchain.gitbook.io/">
-                        <a target="_blank" rel="noreferrer">
-                          Whitepaper
+                        <a target="blank">Whitepaper</a>
+                      </Link>
+                    </li>
+                    <li className="mt-4 flex w-full items-center justify-center">
+                      <Link href="/login">
+                        <a>
+                          <Button
+                            variant="outline"
+                            className="h-8 w-64 bg-opacity-70"
+                            borderColor="bg-gray-40"
+                            bgColor="bg-black"
+                          >
+                            Login / Sign up
+                          </Button>
                         </a>
                       </Link>
                     </li>
