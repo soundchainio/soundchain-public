56be15c569079059ebfeccc44274f3e01919eaa9
Filipe Alcantara <cauepor@gmail.com>
2022-10-03 12:35:12 -0700

    fix(#183445025): remove logs

--- web/src/components/ExploreAll.tsx
+++ web/src/components/ExploreAll.tsx
@@ -17,7 +17,6 @@ interface ExplorePageProps {
 
 export const ExploreAll = ({ searchTerm, setSelectedTab }: ExplorePageProps) => {
   const { data, loading } = useExploreQuery({ variables: { search: searchTerm }, fetchPolicy: 'network-only' })
-  console.log(data)
   const profiles = data?.explore.profiles
   const tracks = data?.explore.tracks
   const { playlistState } = useAudioPlayerContext()
