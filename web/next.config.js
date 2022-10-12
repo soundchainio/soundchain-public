282d5ab942a49602f2daaca71a69ca52a7726a1f
Randal Andrade Nunes <randal.andrade@hotmail.com>
2022-10-12 01:51:47 -0300

    update config

--- web/next.config.js
+++ web/next.config.js
@@ -1,17 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const withPWA = require('next-pwa')
-
-// module.exports = withPWA({
-//   pwa: {
-//     dest: 'public',
-//     disable: true,
-//   },
-//   reactStrictMode: true,
-//   images: {
-//     domains: ['placeimg.com', 'cdn.fakercloud.com', 'soundchain.mypinata.cloud', process.env.UPLOADS_DOMAIN],
-//   },
-// })
-
 module.exports = {
   reactStrictMode: false,
   images: {
