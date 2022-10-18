14e7ffe68f18b1476841358612f2abd6c995771a
Reginaldo Junior <reginaldo.junior696@gmail.com>
2022-10-18 12:05:36 -0300

    fixed typing on debounce fn

--- web/src/utils/debounce.ts
+++ web/src/utils/debounce.ts
@@ -4,7 +4,7 @@ export function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
   func: F,
   waitFor: number,
 ): (...args: Parameters<F>) => void {
-  let timeout: NodeJS.Timeout
+  let timeout: ReturnType<typeof setTimeout>
   return (...args: Parameters<F>): void => {
     clearTimeout(timeout)
     timeout = setTimeout(() => func(...args), waitFor)
