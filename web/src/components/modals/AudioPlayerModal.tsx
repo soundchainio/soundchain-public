521ac2ce567d49eed8ba9d6b80f923484b9aaba1
Randal Andrade Nunes <randal.andrade@hotmail.com>
2022-10-19 22:19:53 -0300

    open playlist button

--- web/src/components/modals/AudioPlayerModal.tsx
+++ web/src/components/modals/AudioPlayerModal.tsx
@@ -47,9 +47,11 @@ export const AudioPlayerModal = () => {
     jumpTo,
     toggleShuffle,
     setPlayerFavorite,
+    isPlaylistOpen,
+    setIsPlaylistOpen
   } = useAudioPlayerContext()
+  
   const [showTotalPlaybackDuration, setShowTotalPlaybackDuration] = useState(true)
-  const [isPlaylistOpen, setIsPlaylistOpen] = useState(false)
   const [isMobile, setIsMobile] = useState(true)
   const me = useMe()
 
