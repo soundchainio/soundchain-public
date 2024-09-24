0ff181f1a9a4c0ed5d6746e20f759876d2d53751
Soundchain <frank@soundchain.io>
2024-09-24 20:57:56 +0000

    add test lambda

---
+++ api/src/lambda/hello.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const hello: APIGatewayProxyHandler = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello' as const,
+    }),
+  };
+};
