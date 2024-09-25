6a64c98045238df1b6445166359e48a8ba822309
Soundchain <frank@soundchain.io>
2024-09-25 22:06:35 +0000

    update lambda config

---
+++ api/src/lambda/graphql/handler.ts
@@ -0,0 +1,30 @@
+import * as Sentry from '@sentry/serverless';
+import { mongoose } from '@typegoose/typegoose';
+import { ApolloServer } from 'apollo-server-lambda';
+import type { Handler } from 'aws-lambda';
+import express from 'express';
+import { config, NODE_ENV } from '../../config';
+
+Sentry.AWSLambda.init({
+  dsn: config.sentry.url,
+  tracesSampleRate: 1.0,
+  environment: NODE_ENV,
+});
+
+const graphqlHandler: Handler = async (...args) => {
+  await mongoose.connect(config.db.url, config.db.options);
+
+  const server = new ApolloServer(config.apollo);
+  const apolloHandler = server.createHandler({
+    expressAppFromMiddleware(middleware) {
+      const app = express();
+      app.use(config.express.middlewares);
+      app.use(middleware);
+      return app;
+    },
+  });
+
+  return apolloHandler(...args);
+};
+
+export const handler = Sentry.AWSLambda.wrapHandler(graphqlHandler);
