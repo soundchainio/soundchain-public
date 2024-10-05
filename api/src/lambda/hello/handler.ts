353249a0f52f57f546b2c407291651e16854f34f
Soundchain <frank@soundchain.io>
2024-10-05 19:44:24 +0000

    test db connection

--- api/src/lambda/hello/handler.ts
+++ api/src/lambda/hello/handler.ts
@@ -1,6 +1,13 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { config } from '../../config';
+import { mongoose } from '@typegoose/typegoose';
 
 export const handler: APIGatewayProxyHandler = async event => {
+  console.log('Connecting to MongoDB');
+  console.log(config.db.url);
+  console.log(config.db.options);
+  await mongoose.connect(config.db.url, config.db.options);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
