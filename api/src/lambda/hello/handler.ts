8e5581d3718560bd68bafcb7546f546a2612062b
Soundchain <frank@soundchain.io>
2024-10-08 15:51:57 +0000

    building with tsc

--- api/src/lambda/hello/handler.ts
+++ api/src/lambda/hello/handler.ts
@@ -1,15 +1,6 @@
-import 'reflect-metadata';
-
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import { config } from '../../config';
-import { mongoose } from '@typegoose/typegoose';
 
 export const handler: APIGatewayProxyHandler = async event => {
-  console.log('Connecting to MongoDB');
-  console.log(config.db.url);
-  console.log(config.db.options);
-  await mongoose.connect(config.db.url, config.db.options);
-
   return {
     statusCode: 200,
     body: JSON.stringify({
