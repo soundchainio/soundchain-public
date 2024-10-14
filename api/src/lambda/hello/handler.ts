8f97c98ea0e2dc4cb835dccb12dceff186a52643
Soundchain <frank@soundchain.io>
2024-10-14 23:26:39 +0000

    fix url

--- api/src/lambda/hello/handler.ts
+++ api/src/lambda/hello/handler.ts
@@ -1,12 +1,11 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
 import { mongoose } from '@typegoose/typegoose';
+import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const handler: APIGatewayProxyHandler = async () => {
   try {
     console.log('Attempting to connect to MongoDB...');
     const url =
       'mongodb://production:8uV53MWUu6DPfdL5@db-soundchain-api-production.cluster-capqvzyh8vvd.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
-
     await mongoose.connect(url);
     console.log('Successfully connected to MongoDB');
 
