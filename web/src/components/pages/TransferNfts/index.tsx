277ad5fd9440f9362c29da4b3df5b56dedf50182
soundchain_io <114355021+soundchainio@users.noreply.github.com>
2024-12-08 10:06:07 -0700

    Update index.tsx

--- web/src/components/pages/TransferNfts/index.tsx
+++ web/src/components/pages/TransferNfts/index.tsx
@@ -188,7 +188,7 @@ export function useTransferNftsControls() {
     gasCheck()
   }, [web3, getCurrentGasPrice])
 
-  const pageSize = 1000
+  const pageSize = 20
   const { data, loading, fetchMore, refetch } = useTracksQuery({
     variables: {
       filter: {
