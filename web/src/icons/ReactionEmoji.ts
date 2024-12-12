4875ad3be0cd2764c6ef72c719711850dd3e992d
soundchain_io <114355021+soundchainio@users.noreply.github.com>
2024-12-12 10:04:18 -0700

    Update ReactionEmoji.ts

--- web/src/icons/ReactionEmoji.ts
+++ web/src/icons/ReactionEmoji.ts
@@ -1,27 +1,35 @@
-import { ReactionType } from 'lib/graphql'
-import { Fire } from './emoji/Fire'
-import { Heart } from './emoji/Heart'
-import { Hundred } from './emoji/Hundred'
-import { Rocket } from './emoji/Rocket'
-import { Sunglasses } from './emoji/Sunglasses'
-import { IconComponent } from './types/IconComponent'
+import { ReactionType } from 'lib/graphql';
+import { Fire } from './emoji/Fire';
+import { Heart } from './emoji/Heart';
+import { Hundred } from './emoji/Hundred';
+import { Rocket } from './emoji/Rocket';
+import { Sunglasses } from './emoji/Sunglasses';
+import { IconComponent } from './types/IconComponent';
 
 interface ReactionEmojiProps extends React.SVGProps<SVGSVGElement> {
-  name: ReactionType
+  name: ReactionType | string; // Allow both predefined and custom emoji names
 }
 
-// We will need a bigger migration if we wanna change the key names
-// of the emojis, it should be something like first, second, third...
-// then we could always rewrite the entire history by changing only
-// the svg file, for now this solution will work
+// Static mapping for default emojis
 const emojiByName: Record<ReactionType, IconComponent> = {
   HAPPY: Hundred,
   HEART: Heart,
   HORNS: Rocket,
   SAD: Fire,
   SUNGLASSES: Sunglasses,
-}
+};
 
 export const ReactionEmoji = ({ name, ...props }: ReactionEmojiProps) => {
-  return emojiByName[name](props)
-}
+  // Check if the reaction is a custom emoji or a default one
+  if (emojiByName[name as ReactionType]) {
+    // Render predefined emoji
+    return emojiByName[name as ReactionType](props);
+  }
+
+  // Render custom emoji from `emoji-mart`
+  return (
+    <span role="img" aria-label="reaction" {...props}>
+      {name}
+    </span>
+  );
+};
