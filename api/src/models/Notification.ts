a65c46d4723cc3ea388ff52e716ccd2bf3832de4
soundchain_io <114355021+soundchainio@users.noreply.github.com>
2024-12-12 11:43:29 -0700

    Update Notification.ts

--- api/src/models/Notification.ts
+++ api/src/models/Notification.ts
@@ -1,23 +1,12 @@
+import { Field, ID } from 'type-graphql';
 import { getModelForClass, modelOptions, prop, Severity } from '@typegoose/typegoose';
 import { ObjectId } from 'mongodb';
-import { Field, ID } from 'type-graphql';
-import { CommentNotificationMetadata } from '../types/CommentNotificationMetadata';
-import { DeletedCommentNotificationMetadata } from '../types/DeletedCommentNotificationMetadata';
-import { DeletedPostNotificationMetadata } from '../types/DeletedPostNotificationMetadata';
-import { FollowerNotificationMetadata } from '../types/FollowerNotificationMetadata';
-import { NewPostNotificationMetadata } from '../types/NewPostNotificationMetadata';
-import { NewVerificationRequestNotificationMetadata } from '../types/NewVerificationRequestNotificationMetadata';
-import { NFTSoldNotificationMetadata } from '../types/NFTSoldNotificationMetadata';
-import { NotificationType } from '../types/NotificationType';
-import { ReactionNotificationMetadata } from '../types/ReactionNotificationMetadata';
-import { TrackWithPriceMetadata } from '../types/TrackWithPriceMetadata';
-import { VerificationRequestNotificationMetadata } from '../types/VerificationRequestNotificationMetadata';
 import { Model } from './Model';
 
 @modelOptions({ options: { allowMixed: Severity.ALLOW } })
 export class Notification extends Model {
-  @Field(() => ID) // Explicitly expose `_id` as a GraphQL ID type
-  readonly _id!: ObjectId;
+  @Field(() => ID) // Exposes the `_id` field in GraphQL
+  readonly _id: ObjectId;
 
   @prop({ required: true })
   type: NotificationType;
