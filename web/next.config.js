5c18061cbd61ff7658c322ba5bf6b2fba3aed2c5
soundchain_io <114355021+soundchainio@users.noreply.github.com>
2025-03-01 11:16:34 -0700

    Redirect root to /home

--- web/next.config.js
+++ web/next.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    reactStrictMode: false,
-    images: {
-        domains: [
-            'placeimg.com',
-            'cdn.fakercloud.com',
-            'soundchain.mypinata.cloud',
-            ...(process.env.UPLOADS_DOMAIN ? [process.env.UPLOADS_DOMAIN] : []), // Ensure it's only added if defined
-            'soundchain.io', // Add this line
-        ],
-    },
-    async redirects() {
+  reactStrictMode: false,
+  images: {
+    domains: [
+      'placeimg.com',
+      'cdn.fakercloud.com',
+      'soundchain.mypinata.cloud',
+      ...(process.env.UPLOADS_DOMAIN ? [process.env.UPLOADS_DOMAIN] :),
+      'soundchain.io',
+    ],
+  },
+  async redirects() {
     return [
       {
         source: '/',
         destination: '/home',
-        permanent: true, // Sends a 308 Permanent Redirect
+        permanent: true, // 301 redirect (permanent)
       },
     ];
   },
