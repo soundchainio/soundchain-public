071991d3c88303b40e6e0829b06f4d93a833d3c4
soundchain_io <114355021+soundchainio@users.noreply.github.com>
2025-06-18 09:57:48 -0700

    Resolve file conflict by renaming api/migrate-mongo-config.ts to migrate-mongo-config-alt.ts

--- api/migrate-mongo-config-alt.ts
+++ api/migrate-mongo-config-alt.ts
@@ -1,10 +1,14 @@
+import { config, DATABASE_SSL_PATH } from './src/config';
+
 const migrationConfig = {
   mongodb: {
-    url: 'mongodb://soundchainadmin:i%7CrmUvwben0gw%245D3%5B%7E0ZLw-tX%7EL@localhost:27017/test',
+    url: config.db.url || 'mongodb://soundchainadmin:i%7CrmUvwben0gw%245D3%5B%7E0ZLw-tX%7EL@localhost:27017/test',
     databaseName: "test",
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      ssl: Boolean(DATABASE_SSL_PATH),
+      sslCA: DATABASE_SSL_PATH && 'global-bundle.pem',
       tls: true,
       tlsCAFile: '/Users/soundchain/soundchain/api/global-bundle.pem',
       authMechanism: 'SCRAM-SHA-1',
@@ -17,7 +21,6 @@ const migrationConfig = {
   migrationsDir: 'migrations',
   changelogCollectionName: 'changelog',
   useFileHash: false,
-  moduleSystem: 'commonjs',
 };
 
 export = migrationConfig;
