0f694fcf30ea826de54c6ccfdb73abb55f950935
soundchain_io <114355021+soundchainio@users.noreply.github.com>
2025-08-04 13:26:31 -0700

    Fix LoginForm.tsx by removing invalid placeholder and event props

--- web/src/components/LoginForm.tsx
+++ web/src/components/LoginForm.tsx
@@ -1,24 +1,24 @@
-import { Button } from 'components/common/Buttons/Button'
-import { InputField } from 'components/InputField'
-import { Form, Formik } from 'formik'
-import * as yup from 'yup'
+import { Button } from 'components/common/Buttons/Button';
+import { InputField } from 'components/InputField';
+import { Form, Formik } from 'formik';
+import * as yup from 'yup';
 
 export interface FormValues {
-  email: string
+  email: string;
 }
-const validationSchema: yup.SchemaOf<FormValues> = yup.object().shape({
+const validationSchema = yup.object().shape({
   email: yup.string().email('Please enter a valid email address').required('Please enter your email address'),
-})
+});
 
 interface LoginFormProps {
-  handleMagicLogin: (values: FormValues) => void
+  handleMagicLogin: (values: FormValues) => void;
 }
 
 export const LoginForm = ({ handleMagicLogin }: LoginFormProps) => {
   return (
     <Formik initialValues={{ email: '' }} validationSchema={validationSchema} onSubmit={handleMagicLogin}>
       {({ isSubmitting }) => (
-        <Form className="flex flex-1 flex-col justify-between" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
+        <Form className="flex flex-1 flex-col justify-between">
           <div>
             <InputField placeholder="Email address" type="email" name="email" />
           </div>
@@ -28,5 +28,5 @@ export const LoginForm = ({ handleMagicLogin }: LoginFormProps) => {
         </Form>
       )}
     </Formik>
-  )
-}
+  );
+};
